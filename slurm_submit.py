06ac21263dd545ae6b853eaa0023cb92d38cc3af
Isaiah Santistevan <isaiahsantistevan@campus-002-118.ucdavis.edu>
2019-12-17 08:52:07 -0800

    Testing something

--- slurm_submit.py
+++ slurm_submit.py
@@ -3,9 +3,9 @@
 #SBATCH --job-name=test_script
 #SBATCH --partition=high2    # peloton node: 32 cores, 7.8 GB per core, 250 GB total
 ##SBATCH --partition=high2m    # peloton high-mem node: 32 cores, 15.6 GB per core, 500 GB total
-#SBATCH --mem=62G    # need to specify memory if you set the number of tasks (--ntasks) below
-##SBATCH --nodes=1    # if you specify this, the number of nodes, do not set memory (--mem) above
-##SBATCH --ntasks-per-node=1    # (MPI) tasks per node
+##SBATCH --mem=62G    # need to specify memory if you set the number of tasks (--ntasks) below
+#SBATCH --nodes=1    # if you specify this, the number of nodes, do not set memory (--mem) above
+#SBATCH --ntasks-per-node=1    # (MPI) tasks per node
 #SBATCH --ntasks=1    # (MPI) tasks total
 #SBATCH --cpus-per-task=1    # (OpenMP) threads per (MPI) task
 #SBATCH --time=00:15:00
