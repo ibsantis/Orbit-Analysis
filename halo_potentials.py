99b140ecb487a9b20c2ad50e554d1d5ecaf5e205
Isaiah Santistevan <isaiahsantistevan@campus-060-098.ucdavis.edu>
2020-02-24 15:02:44 -0800

    Wrote new script to calculate the potentials for the halos in m12i

---
+++ halo_potentials.py
@@ -0,0 +1,106 @@
+#!/usr/bin/python3
+
+"""
+ ==========================================
+ = Progenitor halo star particle tracking =
+ ==========================================
+
+ Written by Isaiah Santistevan (ibsantistevan@ucdavis.edu) during Winter Quarter, 2020
+
+ GOAL:
+    1. Select luminous halos .
+    2. For each halo calculate the mean and median gravitational potential
+       using their member star particles.
+    3. Save the data to a file.
+"""
+
+# Import all of the tools for analysis
+import halo_analysis as halo
+import gizmo_analysis as gizmo
+import utilities as ut
+import orbit_times as ot
+import numpy as np
+import pickle
+import matplotlib
+from matplotlib import pyplot as plt
+from matplotlib import patches
+from scipy.interpolate import interp1d
+print('Read in the tools')
+
+### Set path and initial parameters
+gal1 = 'm12i'
+loc = 'stampede'
+
+if gal1 == 'Romeo':
+    gal2 = 'Juliet'
+    galaxy = 'm12_elvis_'+gal1+gal2
+    resolution = '_res3500'
+    num_gal = 2
+elif gal1 == 'Thelma':
+    gal2 = 'Louise'
+    galaxy = 'm12_elvis_'+gal1+gal2
+    resolution = '_res4000'
+    num_gal = 2
+elif gal1 == 'Romulus':
+    gal2 = 'Remus'
+    galaxy = 'm12_elvis_'+gal1+gal2
+    resolution = '_res4000'
+    num_gal = 2
+else:
+    galaxy = gal1
+    resolution = '_res7100'
+    num_gal = 1
+
+if loc == 'mac':
+    home_dir = '/Users/isaiahsantistevan/simulation'
+else:
+    home_dir = '/home1/05400/ibsantis/scripts'
+simulation_dir = '/scratch/projects/xsede/GalaxiesOnFIRE/metal_diffusion/'+galaxy+resolution
+print('Set paths')
+
+# Read in the entire tree
+snaps = ut.simulation.read_snapshot_times(directory=simulation_dir) # Saves snapshots, redshifts, lookback times, etc. to an array
+halt = halo.io.IO.read_tree(simulation_directory='/scratch/projects/xsede/GalaxiesOnFIRE/metal_diffusion/m12i_res7100')
+
+# Set up array of snapshot values starting from 600 to 0 (redshift 0 to 99)
+ss = np.flip(np.arange(601))
+# Get Luminous halo indices
+lum_ind = ut.array.get_indices(halt['star.mass'], [1e-6, np.inf])
+# Get list of halo inds for each snapshot
+# This has length 601 (from 600 to 0)
+halo_inds = [ut.array.get_indices(halt['snapshot'], ss[i], lum_ind) for i in range(0, len(ss))]
+
+"""
+Method:
+    0. Set up empty lists to save the data to.
+       Each element of this list will contain an array for each snapshot (up to snapshot = 1)
+    1. Read in the particle data at the snapshot.
+    2. For each halo, calculate the mean and median gravitational potential using
+       only the halo's star particle members.
+    3. Save the data to a dictionary.
+"""
+# Set up empty lists
+halo_potentials_mean = []
+halo_potentials_median = []
+
+start = time.time()
+# Loop over snapshots
+# Only go until snapshot = 2 because there are no stars at snapshot = 1
+for i in range(0, 599):
+    part_at_z = gizmo.io.Read.read_snapshots('star', 'snapshot', ss[i], simulation_directory=simulation_dir, properties=['position', 'potential'], assign_host_coordinates=False)
+    halo_potentials_mean.append(np.asarray([np.mean(part_at_z['star']['potential'][halt['star.indices'][halo_inds[i][j]]]) for j in range(0, len(halo_inds[i]))]))
+    halo_potentials_median.append(np.asarray([np.median(part_at_z['star']['potential'][halt['star.indices'][halo_inds[i][j]]]) for j in range(0, len(halo_inds[i]))]))
+end = time.time()
+print('Finished calculating the halo potentials in', end-start, 'seconds')
+
+# Throw the data in a dictionary and save it to a file
+data_dict = dict()
+# Halo indices will be arrays of indices at each snapshot. (has length = 601)
+data_dict['halo.indices'] = halo_inds
+data_dict['halo.potential.mean'] = pot_mean_all
+data_dict['halo.potential.median'] = pot_med_all
+#
+file_save = open(home_dir'/orbit_data/pickles/'+galaxy+'_halo_potentials.p','wb')
+pickle.dump(data_dict, file_save)
+file_save.close()
+print('All done?')
--- slurm_submit.py
+++ slurm_submit.py
@@ -1,25 +1,35 @@
 #!/usr/bin/env python3
 
-#SBATCH --job-name=test_script
-#SBATCH --partition=skx-normal    # peloton node: 32 cores, 7.8 GB per core, 250 GB total
-##SBATCH --partition=high2m    # peloton high-mem node: 32 cores, 15.6 GB per core, 500 GB total
-##SBATCH --mem=62G    # need to specify memory if you set the number of tasks (--ntasks) below
-#SBATCH --nodes=1    # if you specify this, the number of nodes, do not set memory (--mem) above
-#SBATCH --ntasks-per-node=1    # (MPI) tasks per node
-#SBATCH --ntasks=1    # (MPI) tasks total
-#SBATCH --cpus-per-task=1    # (OpenMP) threads per (MPI) task
-#SBATCH --time=00:15:00
-#SBATCH --output=/home1/05400/ibsantis/job_%j.txt
+# Stampede2 SKX node: 48 cores, 4 GB per core, 192 GB total
+#SBATCH --job-name=m12i_halo_potentials
+#SBATCH --partition=skx-normal
+#SBATCH --nodes=1
+#SBATCH --tasks-per-node=48    # MPI tasks per node
+#SBATCH --cpus-per-task=1    # OpenMP threads per MPI task
+#SBATCH --time=03:00:00
+#SBATCH --output=/home1/05400/ibsantis/scripts/gizmo_jobs/m12i_halo_potentials_%j.txt
 #SBATCH --mail-user=ibsantistevan@ucdavis.edu
 #SBATCH --mail-type=fail
 #SBATCH --mail-type=end
+#SBATCH --mail-type=begin
 #SBATCH --account=TG-AST140064
 
+'''
+Submit a Gizmo simulation job to the SLURM queue.
+Check if restart files exist - if yes, start a restart run.
+
+@author: Andrew Wetzel <arwetzel@gmail.com>
+'''
+
 import os
-from utilities.basic import io as ut_io    # if you want to use my print diagnostics 
+
+from utilities.basic import io as ut_io
+
+import os
+from utilities.basic import io as ut_io    # if you want to use my print diagnostics
 
 # print run-time and CPU information
 ScriptPrint = ut_io.SubmissionScriptClass('slurm')
-os.system('python /home1/05400/ibsantis/scripts/orbit_analysis/test_slurm.py')
+os.system('python /home1/05400/ibsantis/scripts/orbit_analysis/halo_potentials.py')
 # print run-time information
 ScriptPrint.print_runtime()
