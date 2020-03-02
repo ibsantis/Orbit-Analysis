e4e20945953529aee2eb5e4b4811b44e7a007b3a
Isaiah Santistevan <isaiahsantistevan@Isaiahs-MBP.attlocal.net>
2020-03-02 15:36:57 -0800

    Changed analysis to run on all of the simulations, changed some naming

--- halo_potentials.py
+++ halo_potentials.py
@@ -29,90 +29,88 @@ from matplotlib import patches
 from scipy.interpolate import interp1d
 print('Read in the tools')
 
-### Set path and initial parameters
-gal1 = 'm12b'
-loc = 'peloton'
+# Loop through this list of simulation names (LG pairs only need to be looped over once)
+names = ['m12b', 'm12c', 'm12f', 'm12i', 'm12m', 'm12w', 'Romeo', 'Thelma', 'Romulus']
 
-if gal1 == 'Romeo':
-    gal2 = 'Juliet'
-    galaxy = 'm12_elvis_'+gal1+gal2
-    resolution = '_res3500'
-    num_gal = 2
-elif gal1 == 'Thelma':
-    gal2 = 'Louise'
-    galaxy = 'm12_elvis_'+gal1+gal2
-    resolution = '_res4000'
-    num_gal = 2
-elif gal1 == 'Romulus':
-    gal2 = 'Remus'
-    galaxy = 'm12_elvis_'+gal1+gal2
-    resolution = '_res4000'
-    num_gal = 2
-else:
-    galaxy = gal1
-    resolution = '_res7100'
-    num_gal = 1
+for n in names:
 
-if loc == 'mac':
-    home_dir = '/Users/isaiahsantistevan/simulation'
-elif loc == 'peloton' and num_gal == 1:
-    home_dir = '/home/ibsantis/scripts'
-    simulation_dir = '/home/awetzel/scratch/'+galaxy+'/'+galaxy+resolution
-elif loc == 'peloton' and num_gal == 2:
-    home_dir = '/home/ibsantis/scripts'
-    simulation_dir = '/home/awetzel/scratch/m12_elvis/'+galaxy+resolution
-else:
-    home_dir = '/home1/05400/ibsantis/scripts'
-    simulation_dir = '/scratch/projects/xsede/GalaxiesOnFIRE/metal_diffusion/'+galaxy+resolution
-print('Set paths')
+    ### Set path and initial parameters
+    gal1 = n
+    loc = 'peloton'
 
-# Read in the entire tree
-snaps = ut.simulation.read_snapshot_times(directory=simulation_dir) # Saves snapshots, redshifts, lookback times, etc. to an array
-halt = halo.io.IO.read_tree(simulation_directory=simulation_dir)
+    if gal1 == 'Romeo':
+        gal2 = 'Juliet'
+        galaxy = 'm12_elvis_'+gal1+gal2
+        resolution = '_res3500'
+        num_gal = 2
+    elif gal1 == 'Thelma':
+        gal2 = 'Louise'
+        galaxy = 'm12_elvis_'+gal1+gal2
+        resolution = '_res4000'
+        num_gal = 2
+    elif gal1 == 'Romulus':
+        gal2 = 'Remus'
+        galaxy = 'm12_elvis_'+gal1+gal2
+        resolution = '_res4000'
+        num_gal = 2
+    else:
+        galaxy = gal1
+        resolution = '_res7100'
+        num_gal = 1
 
-# Set up array of snapshot values starting from 600 to 0 (redshift 0 to 99)
-ss = np.flip(np.arange(601))
-# Get Luminous halo indices
-lum_ind = ut.array.get_indices(halt['star.mass'], [1e-6, np.inf])
-# Get list of halo inds for each snapshot
-# This has length 601 (from 600 to 0)
-halo_inds = [ut.array.get_indices(halt['snapshot'], ss[i], lum_ind) for i in range(0, len(ss))]
+    if loc == 'mac':
+        home_dir = '/Users/isaiahsantistevan/simulation'
+    elif loc == 'peloton' and num_gal == 1:
+        home_dir = '/home/ibsantis/scripts'
+        simulation_dir = '/home/awetzel/scratch/'+galaxy+'/'+galaxy+resolution
+    elif loc == 'peloton' and num_gal == 2:
+        home_dir = '/home/ibsantis/scripts'
+        simulation_dir = '/home/awetzel/scratch/m12_elvis/'+galaxy+resolution
+    else:
+        home_dir = '/home1/05400/ibsantis/scripts'
+        simulation_dir = '/scratch/projects/xsede/GalaxiesOnFIRE/metal_diffusion/'+galaxy+resolution
+    print('Set paths')
 
-"""
-Method:
-    0. Set up empty lists to save the data to.
-       Each element of this list will contain an array for each snapshot (up to snapshot = 1)
-    1. Read in the particle data at the snapshot.
-    2. For each halo, calculate the mean and median gravitational potential using
-       only the halo's star particle members.
-    3. Save the data to a dictionary.
-"""
-# Set up empty lists
-halo_potentials_mean = []
-halo_potentials_median = []
+    # Read in the entire tree
+    snaps = ut.simulation.read_snapshot_times(directory=simulation_dir) # Saves snapshots, redshifts, lookback times, etc. to an array
+    halt = halo.io.IO.read_tree(simulation_directory=simulation_dir)
 
-start = time.time()
-# Loop over snapshots
-# Only go until snapshot = 2 because there are no stars at snapshot = 1
-for i in range(0, 599):
-    part_at_z = gizmo.io.Read.read_snapshots('star', 'snapshot', ss[i], simulation_directory=simulation_dir, properties=['position', 'potential'], assign_host_coordinates=False)
-    #halo_potentials_mean.append(np.asarray([np.mean(part_at_z['star']['potential'][halt['star.indices'][halo_inds[i][j]]]) for j in range(0, len(halo_inds[i]))]))
-    halo_potentials_median.append(np.asarray([np.median(part_at_z['star']['potential'][halt['star.indices'][halo_inds[i][j]]]) for j in range(0, len(halo_inds[i]))]))
-    print(i)
-end = time.time()
-print('Finished calculating the halo potentials in', end-start, 'seconds')
+    # Set up array of snapshot values starting from 600 to 0 (redshift 0 to 99)
+    ss = np.flip(np.arange(601))
+    # Get Luminous halo indices
+    lum_ind = ut.array.get_indices(halt['star.mass'], [1e-6, np.inf])
+    # Get list of halo inds for each snapshot
+    # This has length 601 (from 600 to 0)
+    halo_inds = [ut.array.get_indices(halt['snapshot'], ss[i], lum_ind) for i in range(0, len(ss))]
 
-# Throw the data in a dictionary and save it to a file
-data_dict = dict()
-# Halo indices will be arrays of indices at each snapshot. (has length = 601)
-data_dict['halo.indices'] = np.asarray(halo_inds)
-#data_dict['halo.potential.mean'] = halo_potentials_mean
-data_dict['halo.potentials'] = np.asarray(halo_potentials_median)
-#
-#file_save = open(home_dir+'/orbit_data/pickles/'+galaxy+'_halo_potentials.p','wb')
-#pickle.dump(data_dict, file_save)
-#file_save.close()
+    """
+    Method:
+        0. Set up empty lists to save the data to.
+           Each element of this list will contain an array for each snapshot (up to snapshot = 1)
+        1. Read in the particle data at the snapshot.
+        2. For each halo, calculate the median gravitational potential using
+           only the halo's star particle members.
+        3. Save the data to a dictionary.
+    """
+    # Set up empty lists
+    halo_potentials = []
 
-# Save the file to hdf5 format
-ut.io.file_hdf5(gal1+'_halo_potentials', data_dict)
-print('All done!')
+    start = time.time()
+    # Loop over snapshots
+    # Only go until snapshot = 2 because there are no stars at snapshot = 1
+    for i in range(0, 599):
+        part_at_z = gizmo.io.Read.read_snapshots('star', 'snapshot', ss[i], simulation_directory=simulation_dir, properties='potential', assign_host_coordinates=False)
+        halo_potentials.append(np.asarray([np.median(part_at_z['star']['potential'][halt['star.indices'][halo_inds[i][j]]]) for j in range(0, len(halo_inds[i]))]))
+        print(i)
+    end = time.time()
+    print('Finished calculating the halo potentials in', end-start, 'seconds')
+
+    # Throw the data in a dictionary and save it to a file
+    data_dict = dict()
+    # Halo indices will be arrays of indices at each snapshot. (has length = 601)
+    data_dict['halo.indices'] = np.asarray(halo_inds)
+    data_dict['halo.potentials'] = np.asarray(halo_potentials)
+
+    # Save the file to hdf5 format
+    ut.io.file_hdf5('/home/ibsantis/scripts/orbit_data/pickles/'+galaxy+'_halo_potentials', data_dict)
+    print('All done!')
