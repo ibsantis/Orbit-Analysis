58b09359088de02630f6a5097961127619841db8
Isaiah Santistevan <isaiahsantistevan@Isaiahs-MacBook-Pro.local>
2020-11-02 15:26:04 -0800

    Testing something

--- orbit_io.py
+++ orbit_io.py
@@ -66,7 +66,8 @@ class OrbitAnalysis:
         z0_inds = ut.array.get_indices(tree['snapshot'], 600)
         # Select luminous subhalos at z = 0 and find their progenitor indices
         z0_inds_w_star = ut.array.get_indices(tree['star.mass'], [1, np.inf], z0_inds)
-        self.z0_inds_w_star_prog = tree.prop('progenitor.main.indices', z0_inds_w_star)
+        z0_inds_w_star_prog = tree.prop('progenitor.main.indices', z0_inds_w_star)
+        self.sub_inds = z0_inds_w_star_prog
 
     #def get_luminous_halos(self, tree):
         """
