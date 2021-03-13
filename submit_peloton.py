2fd57b5f2be6945ab07a0f7b8f91503b56ff5378
Isaiah <ibsantistevan@ucdavis.edu>
2021-03-12 18:58:15 -0800

    Running on R&J

--- submit_peloton.py
+++ submit_peloton.py
@@ -1,14 +1,14 @@
 #!/usr/bin/env python3
 
-#SBATCH --job-name=mass_profile_m12z
+#SBATCH --job-name=mass_profile_RJ
 #SBATCH --partition=high2    # peloton node: 32 cores, 7.8 GB per core, 250 GB total
 ##SBATCH --partition=high2m    # peloton high-mem node: 32 cores, 15.6 GB per core, 500 GB total
 #SBATCH --mem=125G
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=32    # MPI tasks per node
 #SBATCH --cpus-per-task=1    # OpenMP threads per MPI task
-#SBATCH --time=12:00:00
-#SBATCH --output=/home/ibsantis/scripts/jobs/mass_profiles/m12z_mass_profile_%j.txt
+#SBATCH --time=18:00:00
+#SBATCH --output=/home/ibsantis/scripts/jobs/mass_profiles/RJ_mass_profile_%j.txt
 #SBATCH --mail-user=ibsantistevan@ucdavis.edu
 #SBATCH --mail-type=fail
 #SBATCH --mail-type=end
