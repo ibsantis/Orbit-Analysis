5ab6a929a77dff931c9fdb47d06ae39d0359cb9c
Isaiah <ibsantistevan@ucdavis.edu>
2021-03-17 08:57:08 -0700

    Increased memory

--- submit_peloton.py
+++ submit_peloton.py
@@ -3,7 +3,7 @@
 #SBATCH --job-name=mass_profile_TL
 #SBATCH --partition=high2    # peloton node: 32 cores, 7.8 GB per core, 250 GB total
 ##SBATCH --partition=high2m    # peloton high-mem node: 32 cores, 15.6 GB per core, 500 GB total
-#SBATCH --mem=125G
+#SBATCH --mem=250G
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=32    # MPI tasks per node
 #SBATCH --cpus-per-task=1    # OpenMP threads per MPI task
