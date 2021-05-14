1bd58ecc5daa0252fd68fcce130f9baa2858c572
Isaiah <ibsantistevan@ucdavis.edu>
2021-05-14 08:52:26 -0700

    Finished adding disk modeling functions.

--- double_exponential_fit.py
+++ double_exponential_fit.py
@@ -40,77 +40,43 @@ density_total = data_vert['density.total']
 mass_total = data_vert['mass.total']
 zs = data_vert['zs']
 #
-##############################################################################
-"""
-    - Define the double exponential density model
 
-        - Fit the model to the radial profile ONLY
-"""
-# Double exponential model
-@custom_model
-def double_exponential_density(r, amp1=1e10, r1=1, amp2=1e10, r2=1):
-    return amp1*np.exp(-r/r1) + amp2*np.exp(-r/r2)
-#
-# Fit the model to the data for various cutoff radii
-model_init = double_exponential_density(bounds={'amp1':(1e7, 5e11), 'r1':(1e-1, 10), 'amp2':(1e8, 5e12), 'r2':(1e-2, 10)})
-fit = LevMarLSQFitter()
-model_de = fit(model_init, rs[1:], density_rad, maxiter=100000)
-print(model_de)
+# Initialize the class (not sure if this is how to explain this?)
+disk_models = model_io.DensityModelFit()
 
-# Inner region
-@custom_model
-def double_exponential_density1(r, amp1=1e9, r1=1):
-    return amp1*np.exp(-r/r1)
-#
-# Fit the model to the data for various cutoff radii
-model_init = double_exponential_density1(bounds={'amp1':(1e7, 5e11), 'r1':(1e-1, 10)})
-fit = LevMarLSQFitter()
-model_de1 = fit(model_init, rs[1:17], density_rad[:16], maxiter=100000)
-print(model_de1)
-#
-# Outer region
-@custom_model
-def double_exponential_density2(r, amp2=1e9, r2=1):
-    return amp2*np.exp(-r/r2)
-#
-# Fit the model to the data for various cutoff radii
-model_init = double_exponential_density2(bounds={'amp2':(-1e8, 5e12), 'r2':(-1e-2, 10)})
-fit = LevMarLSQFitter()
-model_de2 = fit(model_init, rs[17:], density_rad[16:], maxiter=100000)
-print(model_de2)
-#
-model_de = model_de1+model_de2
-print(model_de)
+# Get a fit for hz to the vertical density profile
+disk_v = disk_models.disk_vert_dens_model(distances=zs, densities=density_total, Amp=1e7, hz=1, Amp_bounds=(1e5, 5e11), hz_bounds=(1e-2, 1000))
 
-"""
-    Make a plot of the profiles
-"""
+# Get a fit for the other parameters in the radial density profile
+#disk_r = disk_models.disk_rad_dens_model(distances=rs, densities=density_rad, A_in=1e10, r_in=1, A_out=1e9, r_out=1, A_in_bounds=(1e7,5e11), r_in_bounds=(1e-1,10), A_out_bounds=(1e5, 5e11), r_out_bounds=(1e-2,20))
+disk_r = disk_models.disk_rad_dens_model(distances=rs, densities=density_rad, A_in=1e10, r_in=1, A_out=1e9, r_out=1, A_in_bounds=(1e7,5e11), r_in_bounds=(1e-1,10), A_out_bounds=(1e5, 5e11), r_out_bounds=(1e-2,20), r_cut=8)
+
+# Make a plot of the model and data
 plt.figure(figsize=(10,8))
 plt.plot(rs[1:], density_rad, 'k.', label='Data')
-plt.plot(rs, model_de(rs), '-', label='Two Exponentials')
+plt.plot(rs, disk_r(rs), '-', label='Two Exponentials')
 plt.yscale('log')
 plt.xlim(xmin=0,xmax=20.1)
 plt.ylim(ymin=1e6, ymax=1e10)
 plt.xlabel('R [kpc]', fontsize=28)
 plt.ylabel('$\\rho(R)$ [$M_{\\odot} \ kpc^{-3}$]', fontsize=28)
-plt.title(gal1+', |Z| < 3 kpc', fontsize=28)
+plt.title(sim_data.galaxy+', |Z| < 3 kpc', fontsize=28)
 plt.legend(prop={'size': 18})
 plt.tight_layout()
-plt.savefig(home_dir+'/orbit_data/plots/fitting/double_exponential/'+gal1+'/'+gal1+'_disk_radial_profile.pdf')
+plt.savefig(sim_data.home_dir+'/orbit_data/plots/fitting/double_exponential/'+sim_data.galaxy+'/'+sim_data.galaxy+'_disk_radial_profile.pdf')
 plt.close()
 #
 plt.figure(figsize=(10,8))
-plt.plot(rs[1:], density_rad/model_de(rs[1:]), '-')
+plt.plot(rs[1:], disk_r(rs[1:])/density_rad, '-')
 plt.xlim(xmin=0,xmax=20.1)
 plt.hlines(y=1,xmin=1,xmax=20.1,linestyles='dotted')
 plt.ylim(ymin=0.5, ymax=2)
 plt.xlabel('R [kpc]', fontsize=28)
-plt.ylabel('$\\rho_{\\rm data}(R)/\\rho_{\\rm model}(R)$', fontsize=28)
-plt.title(gal1+', |Z| < 3 kpc', fontsize=28)
+plt.ylabel('$\\rho_{\\rm model}(R)/\\rho_{\\rm sim}(R)$', fontsize=28)
+plt.title(sim_data.galaxy+', |Z| < 3 kpc', fontsize=28)
 plt.tight_layout()
-plt.savefig(home_dir+'/orbit_data/plots/fitting/double_exponential/'+gal1+'/'+gal1+'_disk_radial_profile_ratio.pdf')
+plt.savefig(sim_data.home_dir+'/orbit_data/plots/fitting/double_exponential/'+sim_data.galaxy+'/'+sim_data.galaxy+'_disk_radial_profile_ratio.pdf')
 plt.close()
-
 
 """
     Make a plot of the enclosed mass
@@ -122,7 +88,7 @@ mass_de = np.zeros(len(rs)-1)
 #
 for i in range(0, len(rs)-1):
     area = np.pi*(rs[i+1]**2 - rs[i]**2)
-    mass_de[i] = model_de(rs[i+1])*area
+    mass_de[i] = disk_r(rs[i+1])*area
 #
 plt.figure(figsize=(10,8))
 plt.plot(rs[1:], np.cumsum(mass_rad), 'k.', label='Data')
@@ -132,68 +98,50 @@ plt.xlim(xmin=0, xmax=20.1)
 plt.ylim(ymin=3e9, ymax=2e11)
 plt.xlabel('R [kpc]', fontsize=28)
 plt.ylabel('M(<R) [$M_{\\odot}$]', fontsize=28)
-plt.title(gal1+', |Z| < 3 kpc', fontsize=28)
+plt.title(sim_data.galaxy+', |Z| < 3 kpc', fontsize=28)
 plt.legend(prop={'size': 18})
 plt.tight_layout()
-plt.savefig(home_dir+'/orbit_data/plots/fitting/double_exponential/'+gal1+'/'+gal1+'_enclosed_disk_mass.pdf')
+plt.savefig(sim_data.home_dir+'/orbit_data/plots/fitting/double_exponential/'+sim_data.galaxy+'/'+sim_data.galaxy+'_enclosed_disk_mass.pdf')
 plt.close()
 #
 plt.figure(figsize=(10,8))
-plt.plot(rs[1:], np.cumsum(mass_rad)/np.cumsum(mass_de), '-')
+plt.plot(rs[1:], np.cumsum(mass_de)/np.cumsum(mass_rad), '-')
 plt.hlines(y=1,xmin=0,xmax=20.1,linestyles='dotted')
 plt.xlim(xmin=0, xmax=20.1)
 plt.xlabel('R [kpc]', fontsize=28)
-plt.ylabel('$M_{\\rm data}(<R)/M_{\\rm model}(R)$', fontsize=28)
-plt.title(gal1+', |Z| < 3 kpc', fontsize=28)
+plt.ylabel('$M_{\\rm model}(<R)/M_{\\rm sim}(<R)$', fontsize=28)
+plt.title(sim_data.galaxy+', |Z| < 3 kpc', fontsize=28)
 plt.tight_layout()
-plt.savefig(home_dir+'/orbit_data/plots/fitting/double_exponential/'+gal1+'/'+gal1+'_enclosed_disk_mass_ratio.pdf')
+plt.savefig(sim_data.home_dir+'/orbit_data/plots/fitting/double_exponential/'+sim_data.galaxy+'/'+sim_data.galaxy+'_enclosed_disk_mass_ratio.pdf')
 plt.close()
 
 
-
-##############################################################################
 """
-    - Define the exponential density model
-
-        - Fit the model to the vertical profile ONLY
-"""
-# Single exponential model
-@custom_model
-def exponential_vert_density(z, amp1=1e7, z1=0.1):
-    return amp1*np.exp(-np.abs(z)/z1)
-#
-# Fit the model to the data for various cutoff radii
-model_init = exponential_vert_density(bounds={'amp1':(1e5, 5e11), 'z1':(1e-2, 10)})
-fit = LevMarLSQFitter()
-model_dev = fit(model_init, zs[1:], density_total, maxiter=10000000)
-print(model_dev)
-
-"""
-    Make a plot of the profiles
+    - Plot the vertical density profile
 """
 plt.figure(figsize=(10,8))
 plt.plot(zs[1:], density_total, 'k.', label='Data')
-plt.plot(zs, model_dev(zs), '-', label='Exponential')
+plt.plot(zs, disk_v(zs), '-', label='Exponential')
 plt.yscale('log')
 plt.xlim(xmin=0,xmax=3.1)
 #plt.ylim(ymin=1e6, ymax=1e8)
 plt.xlabel('|Z| [kpc]', fontsize=28)
 plt.ylabel('$\\rho(Z)$ [$M_{\\odot} \ kpc^{-3}$]', fontsize=28)
-plt.title(gal1, fontsize=28)
+plt.title(sim_data.galaxy, fontsize=28)
 plt.legend(prop={'size': 18})
 plt.tight_layout()
-plt.savefig(home_dir+'/orbit_data/plots/fitting/double_exponential/'+gal1+'/'+gal1+'_disk_vertical_profile.pdf')
+plt.savefig(sim_data.home_dir+'/orbit_data/plots/fitting/double_exponential/'+sim_data.galaxy+'/'+sim_data.galaxy+'_disk_vertical_profile.pdf')
 plt.close()
 #
 plt.figure(figsize=(10,8))
-plt.plot(zs[1:-1], density_total[:-1]/model_dev(zs[1:-1]), '-')
+plt.plot(zs[1:-1], disk_v(zs[1:-1])/density_total[:-1], '-')
 plt.xlim(xmin=0,xmax=3.1)
 plt.hlines(y=1,xmin=0,xmax=3.1,linestyles='dotted')
 plt.xlabel('|Z| [kpc]', fontsize=28)
-plt.ylabel('$\\rho_{\\rm data}(Z)/\\rho_{\\rm model}(Z)$', fontsize=28)
-plt.title(gal1, fontsize=28)
+plt.ylabel('$\\rho_{\\rm model}(Z)/\\rho_{\\rm sim}(Z)$', fontsize=28)
+plt.title(sim_data.galaxy, fontsize=28)
 plt.tight_layout()
-plt.savefig(home_dir+'/orbit_data/plots/fitting/double_exponential/'+gal1+'/'+gal1+'_disk_vertical_profile_ratio.pdf')
+plt.savefig(sim_data.home_dir+'/orbit_data/plots/fitting/double_exponential/'+sim_data.galaxy+'/'+sim_data.galaxy+'_disk_vertical_profile_ratio.pdf')
 plt.close()
 
 
--- model_io.py
+++ model_io.py
@@ -115,14 +115,14 @@ class MassModelFit:
 
 class DensityModelFit:
 
-    def __init(self)__:
+    def __init__(self):
         """
         Don't really have anything to put in here yet...
         Leaving blank for now.
         """
         pass
 
-    def disk_rad_dens_model(self):
+    def disk_rad_dens_model(self, distances, densities, A_in, r_in, A_out, r_out, A_in_bounds, r_in_bounds, A_out_bounds, r_out_bounds, r_cut=None, iters=100000):
         """
         DESCRIPTION:
             Blah blah blah
@@ -133,9 +133,37 @@ class DensityModelFit:
         NOTES:
             Yes.
         """
-        pass
+        ############### TEST THISSSSS
+        if r_cut == None:
+            @custom_model
+            def double_exponential_density(r, amp1=A_in, r1=r_in, amp2=A_out, r2=r_out):
+                return amp1*np.exp(-r/r1) + amp2*np.exp(-r/r2)
+            #
+            # Fit the model to the data for various cutoff radii
+            model_init = double_exponential_density(bounds={'amp1':A_in_bounds, 'r1':r_in_bounds, 'amp2':A_out_bounds, 'r2':r_out_bounds})
+            fit = LevMarLSQFitter()
+            model_disk_rad = fit(model_init, distances[1:], densities, maxiter=iters)
+            print(model_disk_rad)
+            #
+            return model_disk_rad
+        else:
+            @custom_model
+            def double_exponential_density(r, amp=A_in, r_s=r_in):
+                return amp*np.exp(-r/r_s)
+            #
+            cut = np.where(distances > r_cut)[0][0]
+            # Fit the model to the data for various cutoff radii
+            model_init_1 = double_exponential_density(bounds={'amp':A_in_bounds, 'r_s':r_in_bounds})
+            model_init_2 = double_exponential_density(bounds={'amp':A_out_bounds, 'r_s':r_out_bounds})
+            fit = LevMarLSQFitter()
+            model_disk_rad_1 = fit(model_init_1, distances[1:cut], densities[:cut-1], maxiter=iters)
+            model_disk_rad_2 = fit(model_init_2, distances[cut:], densities[cut-1:], maxiter=iters)
+            model_disk_rad = model_disk_rad_1+model_disk_rad_2
+            print(model_disk_rad)
+            #
+            return model_disk_rad
 
-    def disk_vert_dens_model(self):
+    def disk_vert_dens_model(self, distances, densities, Amp, hz, Amp_bounds, hz_bounds, iters=100000):
         """
         DESCRIPTION:
             Blah blah blah
@@ -146,7 +174,18 @@ class DensityModelFit:
         NOTES:
             Yes.
         """
-        pass
+        @custom_model
+        def exponential_vert_density(z, amp1=Amp, z1=hz):
+            return amp1*np.exp(-np.abs(z)/z1)
+        #
+        # Fit the model to the data for various cutoff radii
+        model_init = exponential_vert_density(bounds={'amp1':Amp_bounds, 'z1':hz_bounds})
+        fit = LevMarLSQFitter()
+        model_disk_vert = fit(model_init, distances[1:], densities, maxiter=iters)
+        print(model_disk_vert)
+        #
+        return model_disk_vert
+
 
     def halo_nfw_dens_model(self):
         """
