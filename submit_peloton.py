24a13c51ffc3e8ac3c785c2901c0c1b1fd568042
Isaiah <ibsantistevan@ucdavis.edu>
2021-05-25 09:09:22 -0700

    Wrong settings for job

--- submit_peloton.py
+++ submit_peloton.py
@@ -1,14 +1,20 @@
 #!/usr/bin/env python3
 
 #SBATCH --job-name=m12b_mass_profile
-##SBATCH --partition=high2    # peloton node: 32 cores, 7.8 GB per core, 250 GB total
-#SBATCH --partition=high2m    # peloton high-mem node: 32 cores, 15.6 GB per core, 500 GB total
-#SBATCH --mem=240G
-#SBATCH --nodes=1
-#SBATCH --ntasks-per-node=32    # MPI tasks per node
+###SBATCH --partition=high2    # peloton node: 32 cores, 7.8 GB per core, 250 GB total
+##SBATCH --partition=high2m    # peloton high-mem node: 32 cores, 15.6 GB per core, 500 GB total
+##SBATCH --mem=240G
+##SBATCH --nodes=1
+##SBATCH --ntasks-per-node=32    # MPI tasks per node
+##SBATCH --cpus-per-task=1    # OpenMP threads per MPI task
+##SBATCH --time=18:00:00
+##SBATCH --output=/home/ibsantis/scripts/jobs/mass_profiles/m12b_mass_profile_%j.txt
+
+#SBATCH --partition=skx-normal
+#SBATCH --nodes=4
+#SBATCH --tasks-per-node=48    # MPI tasks per node
 #SBATCH --cpus-per-task=1    # OpenMP threads per MPI task
 #SBATCH --time=18:00:00
-##SBATCH --output=/home/ibsantis/scripts/jobs/mass_profiles/m12b_mass_profile_%j.txt
 #SBATCH --output=/home1/05400/ibsantis/scripts/jobs/m12b_mass_profile_%j.txt
 #SBATCH --mail-user=ibsantistevan@ucdavis.edu
 #SBATCH --mail-type=fail
