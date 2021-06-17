0f331c96e5382e4bcf7305f21cb3339c110035ec
Isaiah <ibsantistevan@ucdavis.edu>
2021-06-17 12:17:42 -0700

    New script where I'm trying to parallelize something for the 1st time

---
+++ mass_profile_parallel.py
@@ -0,0 +1,81 @@
+#!/usr/bin/env python3
+​
+# Stampede2 SKX node: 48 cores, 4 (3.5 useable) GB per core, 192 GB total
+#SBATCH --job-name=m12b_mass_profile_parallel
+#SBATCH --partition=skx-normal
+#SBATCH --mem=192G
+#SBATCH --nodes=1
+#SBATCH --ntasks=4    # processes total
+##SBATCH --ntasks-per-node=1    # processes per node
+##SBATCH --cpus-per-task=4    # processes per task
+#SBATCH --time=08:00:00
+#SBATCH --output=/home1/05400/ibsantis/scripts/jobs/m12b_mass_profile_parallel_%j.txt
+#SBATCH --mail-user=ibsantistevan@ucdavis.edu
+#SBATCH --mail-type=fail
+#SBATCH --mail-type=end
+#SBATCH --mail-type=begin
+
+"""
+
+  ==========================
+  = Mass Profile Evolution =
+  ==========================
+
+  Written by Isaiah Santistevan (ibsantistevan@ucdavis.edu) during Summer Quarter, 2021
+
+  Calculate what the mass profile is accounting for ALL particles
+  within bins of spherical r, out to 500 kpc
+
+  Do this every 100 Myr going back to 1 Gyr (11 snapshots total)
+
+"""
+import halo_analysis as halo
+import gizmo_analysis as gizmo
+import utilities as ut
+import numpy as np
+import h5py
+import orbit_io
+print('Read in the tools')
+
+### Set path and initial parameters
+sim_data = orbit_io.OrbitRead(gal1='m12b', location='stampede')
+print('Set paths')
+
+# Set up snapshot array to loop through
+snaps = np.array([600,587,582,578,573,569,564,560,556,551,547,543,538,534,530,525,521,517,513,509,504,484,463,443,423,404,385,365,346,327,308,289,270,250,231,211,190,169,147,124,99,72,42])
+times = np.array([13.8,13.7,13.6,13.5,13.4,13.3,13.2,13.1,13.,12.9,12.8,12.7,12.6,12.5,12.4,12.3,12.2,12.1,12.,11.9,11.8,11.3,10.8,10.3,9.8,9.3,8.8,8.3,7.8,7.3,6.8,6.3,5.8,5.3,4.8,4.3,3.8,3.3,2.8,2.3,1.8,1.3,0.8])
+rs = np.logspace(np.log10(0.1), np.log10(500), 100)
+
+# function from first loop down
+def mass_evolution(snap, sim_data, rs):
+    # Check if one or two hosts
+    if sim_data.num_gal == 1:
+        #
+        mass_array = np.zeros(len(rs)-1)
+        #
+        # Read in the data
+        part = gizmo.io.Read.read_snapshots(['star','gas','dark'], 'snapshot', snap, simulation_directory=sim_data.simulation_dir, assign_hosts_rotation=True)
+        #print('Particles at snapshot {0} read in.'.format(snap))
+        #
+        # Find the enclosed mass of all particles within 0.1 < R < 500 kpc
+        for j in range(0, len(rs)-1):
+            star_inds = ut.array.get_indices(part['star'].prop('host.distance.total'), [rs[j], rs[j+1]])
+            gas_inds = ut.array.get_indices(part['gas'].prop('host.distance.total'), [rs[j], rs[j+1]])
+            dark_inds = ut.array.get_indices(part['dark'].prop('host.distance.total'), [rs[j], rs[j+1]])
+            mass_array[j] = np.sum(part['star']['mass'][star_inds]) + np.sum(part['gas']['mass'][gas_inds]) + np.sum(part['dark']['mass'][dark_inds])
+            print('Done with step', j)
+        #
+        # Save this data to a file
+        ut.io.file_hdf5(file_name_base=sim_data.home_dir+'/orbit_data/hdf5_files/mass_profiles/'+sim_data.galaxy+'_mass_profile_evolution_'+str(snap), dict_or_array_to_write=mass_array, verbose=True)
+
+args_list = [
+    (snapshot, sim_data, rs) for snapshot in snaps
+    ]
+
+ut.io.run_in_parallel(mass_evolution, args_list, proc=4)
+# Try 4 at first, then try 8
+# How many snaps can I read in simultaneously? Divide total mem by snapshots to get proc number
+#
+# proc_num and n_tasks in SBATCH stuff must be equal
+
+print('All done?')
