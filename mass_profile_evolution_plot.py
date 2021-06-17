b5a5b7f1bebeb15f7c660ba4f1c82bb2702e8055
Isaiah <ibsantistevan@ucdavis.edu>
2021-06-17 11:57:51 -0700

    Reverted back to old version before trying to parallelize.

--- mass_profile_evolution_plot.py
+++ mass_profile_evolution_plot.py
@@ -78,3 +78,96 @@ plt.title(sim_data.galaxy, fontsize=32)
 plt.legend(prop={'size': 16}, ncol=2)
 plt.tight_layout()
 plt.savefig(sim_data.home_dir+'/orbit_data/plots/mass_profiles/'+sim_data.galaxy+'_mass_profile_evolution_z0.pdf')
+
+
+#####################################################################################
+
+
+import orbit_io
+import halo_analysis as halo
+import gizmo_analysis as gizmo
+import utilities as ut
+import numpy as np
+import h5py
+import matplotlib
+from matplotlib import pyplot as plt
+from matplotlib import patches
+import pandas as pd
+print('Read in the tools')
+
+### Set path and initial parameters
+sim_data = orbit_io.OrbitRead(gal1='m12i', location='mac')
+print('Set paths')
+
+# Read in the data
+mass_m12b = ut.io.file_hdf5(sim_data.home_dir+'/orbit_data/hdf5_files/mass_profiles/m12b_mass_profile_evolution')
+mass_m12c = ut.io.file_hdf5(sim_data.home_dir+'/orbit_data/hdf5_files/mass_profiles/m12c_mass_profile_evolution')
+mass_m12f = ut.io.file_hdf5(sim_data.home_dir+'/orbit_data/hdf5_files/mass_profiles/m12f_mass_profile_evolution')
+mass_m12i = ut.io.file_hdf5(sim_data.home_dir+'/orbit_data/hdf5_files/mass_profiles/m12i_mass_profile_evolution')
+mass_m12m = ut.io.file_hdf5(sim_data.home_dir+'/orbit_data/hdf5_files/mass_profiles/m12m_mass_profile_evolution')
+mass_m12r = ut.io.file_hdf5(sim_data.home_dir+'/orbit_data/hdf5_files/mass_profiles/m12r_mass_profile_evolution')
+mass_m12w = ut.io.file_hdf5(sim_data.home_dir+'/orbit_data/hdf5_files/mass_profiles/m12w_mass_profile_evolution')
+mass_m12z = ut.io.file_hdf5(sim_data.home_dir+'/orbit_data/hdf5_files/mass_profiles/m12z_mass_profile_evolution')
+mass_Romeo = ut.io.file_hdf5(sim_data.home_dir+'/orbit_data/hdf5_files/mass_profiles/Romeo_mass_profile_evolution')
+mass_Juliet = ut.io.file_hdf5(sim_data.home_dir+'/orbit_data/hdf5_files/mass_profiles/Juliet_mass_profile_evolution')
+mass_Thelma = ut.io.file_hdf5(sim_data.home_dir+'/orbit_data/hdf5_files/mass_profiles/Thelma_mass_profile_evolution')
+mass_Louise = ut.io.file_hdf5(sim_data.home_dir+'/orbit_data/hdf5_files/mass_profiles/Louise_mass_profile_evolution')
+mass_Romulus = ut.io.file_hdf5(sim_data.home_dir+'/orbit_data/hdf5_files/mass_profiles/Romulus_mass_profile_evolution')
+mass_Remus = ut.io.file_hdf5(sim_data.home_dir+'/orbit_data/hdf5_files/mass_profiles/Remus_mass_profile_evolution')
+
+# Cumulatively sum each and put them all in one array
+mass_prof_all = np.array([np.cumsum(mass_m12b['mass.profile'],axis=1),\
+                          np.cumsum(mass_m12c['mass.profile'],axis=1),\
+                          np.cumsum(mass_m12f['mass.profile'],axis=1),\
+                          np.cumsum(mass_m12i['mass.profile'],axis=1),\
+                          np.cumsum(mass_m12m['mass.profile'],axis=1),\
+                          np.cumsum(mass_m12r['mass.profile'],axis=1),\
+                          np.cumsum(mass_m12w['mass.profile'],axis=1),\
+                          np.cumsum(mass_m12z['mass.profile'],axis=1),\
+                          np.cumsum(mass_Romeo['mass.profile'],axis=1),\
+                          np.cumsum(mass_Juliet['mass.profile'],axis=1),\
+                          np.cumsum(mass_Thelma['mass.profile'],axis=1),\
+                          np.cumsum(mass_Louise['mass.profile'],axis=1),\
+                          np.cumsum(mass_Romulus['mass.profile'],axis=1),\
+                          np.cumsum(mass_Remus['mass.profile'],axis=1)])
+
+# Take the average
+mass_prof_med_all = np.median(mass_prof_all, axis=0)
+mass_prof_avg_all = np.average(mass_prof_med_all, axis=0)
+
+# Plot each snapshot mass profile to the average mass profile
+rs = np.logspace(np.log10(0.1), np.log10(500), 100)
+#
+def color_cycle(cycle_length=len(mass_m12b['time']), cmap_name='plasma', low=0, high=1):
+    cmap=plt.get_cmap(cmap_name)
+    colors = cmap(np.linspace(low, high, cycle_length))
+    return colors
+colorss = color_cycle(len(mass_m12b['time']), cmap_name='plasma', low=0, high=1)
+#
+plt.rcParams["font.family"] = "serif"
+plt.figure(figsize=(10, 12))
+#
+for i in range(0, len(mass_prof_med_all)):
+    plt.plot(rs, mass_prof_med_all[i]/mass_prof_avg_all, color=colorss[i], label=str(mass_m12b['time'][i])+' Gyr')
+plt.xlim(5, 500)
+#plt.ylim(0.97, 1.04)
+plt.hlines(1, 0.1, 500, color='k', alpha=0.8, linestyles='dotted', zorder=100)
+plt.xscale('log')
+plt.xlabel('r [kpc]', fontsize=32)
+plt.ylabel('$M_{\\rm med}$(<r) / $M_{\\rm med,avg}$(<r)', fontsize=32)
+#plt.title(sim_data.galaxy, fontsize=32)
+plt.legend(prop={'size': 16}, ncol=2)
+plt.tight_layout()
+#plt.savefig(sim_data.home_dir+'/orbit_data/plots/mass_profiles/'+sim_data.galaxy+'_mass_profile_evolution.pdf')
+
+
+
+# Sigma percents
+onesigp = 84.13
+onesigm = 15.87
+#
+twosigp = 97.72
+twosigm = 2.28
+#
+thrsigp = 99.87
+thrsigm = 0.13
