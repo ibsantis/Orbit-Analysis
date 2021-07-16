5b68247b560c9770daf67ced289c9bb2cd8ac660
Isaiah <ibsantistevan@ucdavis.edu>
2021-07-16 10:35:18 -0700

    Trying to run mass_profile_evolution parallel

--- submit_peloton.py
+++ submit_peloton.py
@@ -2,25 +2,13 @@
 
 #SBATCH --job-name=m12b_mass_profile
 ###SBATCH --partition=high2    # peloton node: 32 cores, 7.8 GB per core, 250 GB total
-##SBATCH --partition=high2m    # peloton high-mem node: 32 cores, 15.6 GB per core, 500 GB total
-##SBATCH --mem=240G
-##SBATCH --nodes=1
-##SBATCH --ntasks-per-node=32    # MPI tasks per node
-##SBATCH --cpus-per-task=1    # OpenMP threads per MPI task
-##SBATCH --time=18:00:00
-##SBATCH --output=/home/ibsantis/scripts/jobs/mass_profiles/m12b_mass_profile_%j.txt
-
-#SBATCH --partition=skx-normal
+#SBATCH --partition=high2m    # peloton high-mem node: 32 cores, 15.6 GB per core, 500 GB total
+#SBATCH --mem=240G
 #SBATCH --nodes=4
-#SBATCH --tasks-per-node=48    # MPI tasks per node
+#SBATCH --ntasks-per-node=32    # MPI tasks per node
 #SBATCH --cpus-per-task=1    # OpenMP threads per MPI task
-#SBATCH --time=18:00:00
-#SBATCH --output=/home1/05400/ibsantis/scripts/jobs/m12b_mass_profile_%j.txt
-#SBATCH --mail-user=ibsantistevan@ucdavis.edu
-#SBATCH --mail-type=fail
-#SBATCH --mail-type=end
-#SBATCH --mail-type=begin
-
+#SBATCH --time=24:00:00
+#SBATCH --output=/home/ibsantis/scripts/jobs/mass_profiles/m12b_mass_profile_%j.txt
 '''
 Submit a job to the SLURM queue.
 @author: Isaiah Santistevan <ibsantistevan@ucdavis.edu>
