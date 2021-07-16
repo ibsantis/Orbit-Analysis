02b9fff41957c3cf5aa00418d1dc7486ed1210aa
Isaiah <ibsantistevan@ucdavis.edu>
2021-07-16 11:04:18 -0700

    Another possible fix?

--- mass_profile_parallel.py
+++ mass_profile_parallel.py
@@ -1,17 +1,18 @@
 #!/usr/bin/env python3
 ​
-#SBATCH --job-name=m12b_mass_profile_parallel
-#SBATCH --partition=skx-normal
-#SBATCH --nodes=1
-#SBATCH --ntasks-per-node=4    # processes total
-#SBATCH --cpus-per-task=1    # processes per task
-#SBATCH --time=08:00:00
-#SBATCH --output=/home1/05400/ibsantis/scripts/jobs/m12b_mass_profile_parallel_%j.txt
+#SBATCH --job-name=m12b_mass_profile
+###SBATCH --partition=high2    # peloton node: 32 cores, 7.8 GB per core, 250 GB total
+#SBATCH --partition=high2m    # peloton high-mem node: 32 cores, 15.6 GB per core, 500 GB total
+#SBATCH --mem=240G
+#SBATCH --nodes=2
+#SBATCH --ntasks-per-node=32    # MPI tasks per node
+#SBATCH --cpus-per-task=1    # OpenMP threads per MPI task
+#SBATCH --time=24:00:00
+#SBATCH --output=/home/ibsantis/scripts/jobs/mass_profiles/m12b_mass_profile_%j.txt
 #SBATCH --mail-user=ibsantistevan@ucdavis.edu
 #SBATCH --mail-type=fail
 #SBATCH --mail-type=end
 #SBATCH --mail-type=begin
-#SBATCH --account=TG-AST140064
 
 """
 
