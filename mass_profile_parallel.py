bd9c4c55cc20fd90b311d415ca98c5819fef99af
Isaiah <ibsantistevan@ucdavis.edu>
2021-07-19 14:15:43 -0700

    Tried a fix to the sbatch commands

--- mass_profile_parallel.py
+++ mass_profile_parallel.py
@@ -3,9 +3,10 @@
 ###SBATCH --partition=high2    # peloton node: 32 cores, 7.8 GB per core, 250 GB total
 #SBATCH --partition=high2m    # peloton high-mem node: 32 cores, 15.6 GB per core, 500 GB total
 #SBATCH --mem=240G
-#SBATCH --nodes=2
-#SBATCH --ntasks-per-node=32    # MPI tasks per node
-#SBATCH --cpus-per-task=1    # OpenMP threads per MPI task
+#SBATCH --nodes=1
+##SBATCH --ntasks-per-node=8    # MPI tasks per node
+#SBATCH --ntasks=8    # processes total
+##SBATCH --cpus-per-task=1    # OpenMP threads per MPI task
 #SBATCH --time=24:00:00
 #SBATCH --output=/home/ibsantis/scripts/jobs/mass_profiles/m12b_mass_profile_%j.txt
 #SBATCH --mail-user=ibsantistevan@ucdavis.edu
