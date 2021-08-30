fb4f4f7930203442efb5dc6c2a486ad02876cf9e
Isaiah <ibsantistevan@ucdavis.edu>
2021-08-30 15:28:13 -0700

    Running on m12z

--- subhalo_potentials.py
+++ subhalo_potentials.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python3
-#SBATCH --job-name=m12w_potential
+#SBATCH --job-name=m12z_potential
 #SBATCH --partition=high2m    # peloton high-mem node: 32 cores, 15.6 GB per core, 500 GB total
 #SBATCH --mem=240G
 #SBATCH --nodes=1
 #SBATCH --ntasks=1    # processes total
 #SBATCH --time=04:00:00
-#SBATCH --output=/home/ibsantis/scripts/jobs/potentials/m12w_potential_%j.txt
+#SBATCH --output=/home/ibsantis/scripts/jobs/potentials/m12z_potential_%j.txt
 #SBATCH --mail-user=ibsantistevan@ucdavis.edu
 #SBATCH --mail-type=fail
 #SBATCH --mail-type=end
@@ -43,7 +43,7 @@ from matplotlib import patches
 print('Read in the tools')
 
 ### Set path and initial parameters
-sim_data = orbit_io.OrbitRead(gal1='m12w', location='peloton')
+sim_data = orbit_io.OrbitRead(gal1='m12z', location='peloton')
 print('Set paths')
 
 # Read in the snapshot dictionary, halo tree, and z = 0 snapshot
