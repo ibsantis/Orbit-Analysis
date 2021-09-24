2bf736c82e8dc4c5007c00012491227ced2ef7f0
Isaiah <ibsantistevan@ucdavis.edu>
2021-09-24 13:21:25 -0700

    Running on m12w

--- subhalo_potentials.py
+++ subhalo_potentials.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python3
-#SBATCH --job-name=m12r_potential
+#SBATCH --job-name=m12w_potential
 #SBATCH --partition=high2m    # peloton high-mem node: 32 cores, 15.6 GB per core, 500 GB total
 #SBATCH --mem=480G
 #SBATCH --nodes=1
 #SBATCH --ntasks=1    # processes total
 #SBATCH --time=01:00:00
-#SBATCH --output=/home/ibsantis/scripts/jobs/potentials/m12r_potential_%j.txt
+#SBATCH --output=/home/ibsantis/scripts/jobs/potentials/m12w_potential_%j.txt
 #SBATCH --mail-user=ibsantistevan@ucdavis.edu
 #SBATCH --mail-type=fail
 #SBATCH --mail-type=end
@@ -43,7 +43,7 @@ from matplotlib import pyplot as plt
 print('Read in the tools')
 
 ### Set path and initial parameters
-sim_data = orbit_io.OrbitRead(gal1='m12r', location='peloton')
+sim_data = orbit_io.OrbitRead(gal1='m12w', location='peloton')
 print('Set paths')
 #
 if sim_data.num_gal == 1:
