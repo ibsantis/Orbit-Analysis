7b5be28b5c2e19fce3c172d73826d6057c7183f0
Isaiah <ibsantistevan@ucdavis.edu>
2021-09-27 15:03:00 -0700

    Edited and cleaned up a little

--- satellite_statistics_table.py
+++ satellite_statistics_table.py
@@ -35,25 +35,27 @@ print('Set paths')
 
 # Initialize the classes, read in the data, and create data masks
 summary = summary_io.SummaryDataSort()
-data_total = summary.data_read(directory=sim_data.home_dir, hosts='all', sim_type='baryon')
-masks_infall = summary.data_mask(data_total, peri_sim=False, peri_model=False, hosts='all')
+#data_total = summary.data_read(directory=sim_data.home_dir, hosts='all', sim_type='baryon')
+#data_total = summary.data_read(directory=sim_data.home_dir, hosts='all', sim_type='all_baryon')
+data_total = summary.data_read(directory=sim_data.home_dir, hosts='iso_no_z', sim_type='dmo')
+masks_infall = summary.data_mask(data_total, peri_sim=False, peri_model=False, hosts='iso_no_z')
 summary_plot = summary_io.SummaryDataPlot()
 
 # Count the number of satellites that have ever passed within R200
-for name in summary.host_names['all']:
+for name in summary.host_names['iso_no_z']:
     print(np.sum(masks_infall[name]))
 
 # Count the number of satellites that have ever experienced pericenter
-for name in summary.host_names['all']:
+for name in summary.host_names['iso_no_z']:
     print(np.sum(data_total[name]['pericenter.check.sim']))
 
 # Count the number of satellites within R200, but not experienced peri
-for name in summary.host_names['all']:
+for name in summary.host_names['iso_no_z']:
     print(np.sum(~data_total[name]['pericenter.check.sim']*masks_infall[name]))
     print(data_total[name]['dtot.sim'][~data_total[name]['pericenter.check.sim']*masks_infall[name]][:,0])
 
 # Counting the number of satellites with more than 1 pericenter
-for name in summary.host_names['all']:
+for name in summary.host_names['iso_no_z']:
     count = 0
     for n in range(0, len(data_total[name]['pericenter.dist.sim'])):
         mask = (data_total[name]['pericenter.dist.sim'][n] != -1)
@@ -62,7 +64,7 @@ for name in summary.host_names['all']:
     print(name, count)
 
 # Count how many satellites have the recent pericenter as the minimum
-for name in summary.host_names['all']:
+for name in summary.host_names['iso_no_z']:
     count = 0
     for n in range(0, len(data_total[name]['pericenter.dist.sim'])):
         mask = (data_total[name]['pericenter.dist.sim'][n] != -1)
@@ -72,17 +74,17 @@ for name in summary.host_names['all']:
     print(name, count)
 
 # Count how many satellites have peris in sims but not model
-for name in summary.host_names['all']:
+for name in summary.host_names['iso_no_z']:
     count = np.sum(data_total[name]['infall.check']*data_total[name]['pericenter.check.sim']*~data_total[name]['pericenter.check.galpy'])
     print(name, count)
 
 # Count how many satellites have peris in model but not sim
-for name in summary.host_names['all']:
+for name in summary.host_names['iso_no_z']:
     count = np.sum(data_total[name]['infall.check']*~data_total[name]['pericenter.check.sim']*data_total[name]['pericenter.check.galpy'])
     print(name, count)
 
 # For the satellites that have multiple pericenters, count how many have fractional differences > 10% (between recent and minimum)
-for name in summary.host_names['all']:
+for name in summary.host_names['iso_no_z']:
     data_recent = []
     data_min = []
     for i in range(0, len(data_total[name]['pericenter.dist.sim'][masks_infall[name]])):
@@ -95,8 +97,15 @@ for name in summary.host_names['all']:
     #
     print(np.sum((d_peri_recent-d_peri_min)/d_peri_recent > 0.1))
 
+# Print out the most massive sat stellar mass + halo mass, and see if they're the same subhalo
+for i in summary.host_names['all']:
+    print('The maximum stellar mass in {0} is {1} with index {2}'.format(i, data_total[i]['Mstar.z0'][masks_infall[i]].max(), np.where(data_total[i]['Mstar.z0'][masks_infall[i]].max() == data_total[i]['Mstar.z0'][masks_infall[i]])[0][0]))
+    #
+    print('The maximum peak halo mass in {0} is {1} with index {2}'.format(i, data_total[i]['Mhalo.peak'][masks_infall[i]].max(), np.where(data_total[i]['Mhalo.peak'][masks_infall[i]].max() == data_total[i]['Mhalo.peak'][masks_infall[i]])[0][0]))
+    #
+    print('\n')
 
-# Same thing, but saving all data for plotting
+# Recent vs Minimum pericenter plotting stuff
 data_recent = []
 data_min = []
 for name in summary.host_names:
