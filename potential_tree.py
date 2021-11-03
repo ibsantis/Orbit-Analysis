5d0e55a69d3db8e91b625d8deeee89d6059dc40d
Isaiah <ibsantistevan@ucdavis.edu>
2021-11-03 14:07:33 -0700

    Running on m12i

--- potential_tree.py
+++ potential_tree.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python3
-#SBATCH --job-name=m12f_potentials_dmo
+#SBATCH --job-name=m12i_potentials_dmo
 #SBATCH --partition=high2m    # peloton high-mem node: 32 cores, 15.6 GB per core, 500 GB total
 #SBATCH --mem=500G
 #SBATCH --nodes=1
 #SBATCH --ntasks=4    # processes total
 #SBATCH --time=01:00:00
-#SBATCH --output=/home/ibsantis/scripts/jobs/potentials/m12f_potentials_dmo_%j.txt
+#SBATCH --output=/home/ibsantis/scripts/jobs/potentials/m12i_potentials_dmo_%j.txt
 #SBATCH --mail-user=ibsantistevan@ucdavis.edu
 #SBATCH --mail-type=fail
 #SBATCH --mail-type=end
@@ -46,7 +46,7 @@ print('Read in the tools')
 
 ### Set path and initial parameters
 #sim_data = orbit_io.OrbitRead(gal1='m12b', location='peloton')
-sim_data = orbit_io.OrbitRead(gal1='m12f', location='peloton', dmo=True)
+sim_data = orbit_io.OrbitRead(gal1='m12i', location='peloton', dmo=True)
 print('Set paths')
 
 if sim_data.num_gal == 1:
