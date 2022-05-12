30b3262ccfd51305b17025f3fca2fb5d8cb72f81
Isaiah <ibsantistevan@ucdavis.edu>
2022-05-12 13:20:29 -0700

    Added plots showing the mass profile of the median across all hosts

--- mass_profile_evolution_plot.py
+++ mass_profile_evolution_plot.py
@@ -70,7 +70,6 @@ plt.savefig(sim_data.home_dir+'/orbit_data/plots/mass_profiles/'+sim_data.galaxy
 
 plt.rcParams["font.family"] = "serif"
 plt.figure(figsize=(10, 12))
-#
 for i in inds:
     plt.plot(rs[1:], np.cumsum(mass_data['mass.profile'][i])/np.cumsum(mass_data['mass.profile'][0]), color=colorss[i], label=str(times[i])+' Gyr ago')
 plt.xlim(5, 350)
@@ -104,20 +103,20 @@ sim_data = orbit_io.OrbitRead(gal1='m12i', location='mac')
 print('Set paths')
 
 # Read in the data
-mass_m12b = ut.io.file_hdf5(sim_data.home_dir+'/orbit_data/hdf5_files/mass_profiles/m12b_mass_profile_evolution')
-mass_m12c = ut.io.file_hdf5(sim_data.home_dir+'/orbit_data/hdf5_files/mass_profiles/m12c_mass_profile_evolution')
-mass_m12f = ut.io.file_hdf5(sim_data.home_dir+'/orbit_data/hdf5_files/mass_profiles/m12f_mass_profile_evolution')
-mass_m12i = ut.io.file_hdf5(sim_data.home_dir+'/orbit_data/hdf5_files/mass_profiles/m12i_mass_profile_evolution')
-mass_m12m = ut.io.file_hdf5(sim_data.home_dir+'/orbit_data/hdf5_files/mass_profiles/m12m_mass_profile_evolution')
-mass_m12r = ut.io.file_hdf5(sim_data.home_dir+'/orbit_data/hdf5_files/mass_profiles/m12r_mass_profile_evolution')
-mass_m12w = ut.io.file_hdf5(sim_data.home_dir+'/orbit_data/hdf5_files/mass_profiles/m12w_mass_profile_evolution')
-mass_m12z = ut.io.file_hdf5(sim_data.home_dir+'/orbit_data/hdf5_files/mass_profiles/m12z_mass_profile_evolution')
-mass_Romeo = ut.io.file_hdf5(sim_data.home_dir+'/orbit_data/hdf5_files/mass_profiles/Romeo_mass_profile_evolution')
-mass_Juliet = ut.io.file_hdf5(sim_data.home_dir+'/orbit_data/hdf5_files/mass_profiles/Juliet_mass_profile_evolution')
-mass_Thelma = ut.io.file_hdf5(sim_data.home_dir+'/orbit_data/hdf5_files/mass_profiles/Thelma_mass_profile_evolution')
-mass_Louise = ut.io.file_hdf5(sim_data.home_dir+'/orbit_data/hdf5_files/mass_profiles/Louise_mass_profile_evolution')
-mass_Romulus = ut.io.file_hdf5(sim_data.home_dir+'/orbit_data/hdf5_files/mass_profiles/Romulus_mass_profile_evolution')
-mass_Remus = ut.io.file_hdf5(sim_data.home_dir+'/orbit_data/hdf5_files/mass_profiles/Remus_mass_profile_evolution')
+mass_m12b = ut.io.file_hdf5(sim_data.home_dir+'/orbit_data/hdf5_files/mass_profiles/m12b_full_mass_profile')
+mass_m12c = ut.io.file_hdf5(sim_data.home_dir+'/orbit_data/hdf5_files/mass_profiles/m12c_full_mass_profile')
+mass_m12f = ut.io.file_hdf5(sim_data.home_dir+'/orbit_data/hdf5_files/mass_profiles/m12f_full_mass_profile')
+mass_m12i = ut.io.file_hdf5(sim_data.home_dir+'/orbit_data/hdf5_files/mass_profiles/m12i_full_mass_profile')
+mass_m12m = ut.io.file_hdf5(sim_data.home_dir+'/orbit_data/hdf5_files/mass_profiles/m12m_full_mass_profile')
+mass_m12r = ut.io.file_hdf5(sim_data.home_dir+'/orbit_data/hdf5_files/mass_profiles/m12r_full_mass_profile')
+mass_m12w = ut.io.file_hdf5(sim_data.home_dir+'/orbit_data/hdf5_files/mass_profiles/m12w_full_mass_profile')
+mass_m12z = ut.io.file_hdf5(sim_data.home_dir+'/orbit_data/hdf5_files/mass_profiles/m12z_full_mass_profile')
+mass_Romeo = ut.io.file_hdf5(sim_data.home_dir+'/orbit_data/hdf5_files/mass_profiles/Romeo_full_mass_profile')
+mass_Juliet = ut.io.file_hdf5(sim_data.home_dir+'/orbit_data/hdf5_files/mass_profiles/Juliet_full_mass_profile')
+mass_Thelma = ut.io.file_hdf5(sim_data.home_dir+'/orbit_data/hdf5_files/mass_profiles/Thelma_full_mass_profile')
+mass_Louise = ut.io.file_hdf5(sim_data.home_dir+'/orbit_data/hdf5_files/mass_profiles/Louise_full_mass_profile')
+mass_Romulus = ut.io.file_hdf5(sim_data.home_dir+'/orbit_data/hdf5_files/mass_profiles/Romulus_full_mass_profile')
+mass_Remus = ut.io.file_hdf5(sim_data.home_dir+'/orbit_data/hdf5_files/mass_profiles/Remus_full_mass_profile')
 
 # Cumulatively sum each and put them all in one array
 mass_prof_all = np.array([np.cumsum(mass_m12b['mass.profile'],axis=1),\
@@ -135,9 +134,18 @@ mass_prof_all = np.array([np.cumsum(mass_m12b['mass.profile'],axis=1),\
                           np.cumsum(mass_Romulus['mass.profile'],axis=1),\
                           np.cumsum(mass_Remus['mass.profile'],axis=1)])
 
+
+mass_prof_all = np.array([np.cumsum(mass_m12f['mass.profile'],axis=1),\
+                          np.cumsum(mass_m12i['mass.profile'],axis=1),\
+                          np.cumsum(mass_m12m['mass.profile'],axis=1),\
+                          ])
+
+
 # Take the average
 mass_prof_med_all = np.median(mass_prof_all, axis=0)
-mass_prof_avg_all = np.average(mass_prof_med_all, axis=0)
+mass_prof_avg_all = np.average(mass_prof_med_all, axis=0) # average over all snapshots
+mass_prof_avg_500M = np.average(mass_prof_med_all[:6], axis=0) # average over last 500 Myr
+mass_prof_avg_1G = np.average(mass_prof_med_all[:11], axis=0) # average over last 1 Gyr
 
 # Plot each snapshot mass profile to the average mass profile
 rs = np.logspace(np.log10(0.1), np.log10(500), 100)
@@ -148,22 +156,39 @@ def color_cycle(cycle_length=len(mass_m12b['time']), cmap_name='plasma', low=0, 
     return colors
 colorss = color_cycle(len(mass_m12b['time']), cmap_name='plasma', low=0, high=1)
 #
-plt.rcParams["font.family"] = "serif"
-plt.figure(figsize=(10, 12))
+inds = np.arange(len(mass_m12b['time']))[::2]
+times = np.around(13.8-mass_m12b['time'], decimals=2)
 #
-for i in range(0, len(mass_prof_med_all)):
-    plt.plot(rs, mass_prof_med_all[i]/mass_prof_avg_all, color=colorss[i], label=str(mass_m12b['time'][i])+' Gyr')
-plt.xlim(5, 500)
-#plt.ylim(0.97, 1.04)
+plt.rcParams["font.family"] = "serif"
+plt.figure(figsize=(10, 8))
+for i in inds:
+    #plt.plot(rs[1:], mass_prof_med_all[i]/mass_prof_avg_1G, color=colorss[i], label=str(times[i])+' Gyr ago')
+    plt.plot(rs[1:], mass_prof_med_all[i]/mass_prof_med_all[0], color=colorss[i], label=str(times[i])+' Gyr ago')
+plt.xlim(5, 350)
+plt.ylim(0, 1.2)
 plt.hlines(1, 0.1, 500, color='k', alpha=0.8, linestyles='dotted', zorder=100)
 plt.xscale('log')
 plt.xlabel('r [kpc]', fontsize=32)
-plt.ylabel('$M_{\\rm med}$(<r) / $M_{\\rm med,avg}$(<r)', fontsize=32)
-#plt.title(sim_data.galaxy, fontsize=32)
-plt.legend(prop={'size': 16}, ncol=2)
+#plt.ylabel('$M$(<r) / $M_{\\rm avg}$(<r)', fontsize=32)
+plt.ylabel('$M$(<r) / $M_{z=0}$(<r)', fontsize=32)
+#plt.title('Average over last 11.5 Gyr', fontsize=32)
+#plt.title('Average over last 500 Myr', fontsize=32)
+#plt.title('Average over last 1 Gyr', fontsize=32)
+plt.title(' ', fontsize=32)
+#
+cmap = plt.get_cmap('plasma', len(mass_m12b['time']))
+norm = matplotlib.colors.Normalize(vmin=times[-1], vmax=times[0])
+sm = plt.cm.ScalarMappable(cmap=cmap, norm=norm)
+#sm.set_array([])
+cbar = plt.colorbar(sm)
+cbar.set_label('Lookback time [Gyr]', fontsize=28)
+#
 plt.tight_layout()
-#plt.savefig(sim_data.home_dir+'/orbit_data/plots/mass_profiles/'+sim_data.galaxy+'_mass_profile_evolution.pdf')
-
+#plt.savefig(sim_data.home_dir+'/orbit_data/plots/mass_profiles/mass_profile_evolution_avg_all.pdf')
+#plt.savefig(sim_data.home_dir+'/orbit_data/plots/mass_profiles/mass_profile_evolution_avg_500Myr.pdf')
+#plt.savefig(sim_data.home_dir+'/orbit_data/plots/mass_profiles/mass_profile_evolution_avg_1Gyr.pdf')
+plt.savefig(sim_data.home_dir+'/orbit_data/plots/mass_profiles/mass_profile_evolution_z0.pdf')
+plt.close()
 
 
 # Sigma percents
@@ -175,3 +200,36 @@ twosigm = 2.28
 #
 thrsigp = 99.87
 thrsigm = 0.13
+#
+upper_one = np.percentile((mass_prof_med_all/mass_prof_med_all[0]), onesigp, axis=0)
+lower_one = np.percentile((mass_prof_med_all/mass_prof_med_all[0]), onesigm, axis=0)
+upper_two = np.percentile((mass_prof_med_all/mass_prof_med_all[0]), twosigp, axis=0)
+lower_two = np.percentile((mass_prof_med_all/mass_prof_med_all[0]), twosigm, axis=0)
+upper_thr = np.percentile((mass_prof_med_all/mass_prof_med_all[0]), thrsigp, axis=0)
+lower_thr = np.percentile((mass_prof_med_all/mass_prof_med_all[0]), thrsigm, axis=0)
+
+colorss = ['#93E9BE', '#008080', '#0080FF']
+
+plt.rcParams["font.family"] = "serif"
+plt.figure(figsize=(10, 8))
+plt.fill_between(rs[1:], upper_thr, lower_thr, color='#9966cc', alpha=0.3)
+plt.fill_between(rs[1:], upper_two, lower_two, color='#9966cc', alpha=0.5)
+plt.fill_between(rs[1:], upper_one, lower_one, color='#9966cc', alpha=1)
+plt.xlim(5, 350)
+plt.ylim(0, 1.2)
+plt.hlines(1, 0.1, 500, color='k', alpha=1, linestyles='dotted', zorder=100)
+plt.xscale('log')
+plt.xlabel('r [kpc]', fontsize=32)
+#plt.ylabel('$M$(<r) / $M_{\\rm avg}$(<r)', fontsize=32)
+plt.ylabel('$M$(<r) / $M_{z=0}$(<r)', fontsize=32)
+#plt.title('Average over last 11.5 Gyr', fontsize=32)
+#plt.title('Average over last 500 Myr', fontsize=32)
+#plt.title('Average over last 1 Gyr', fontsize=32)
+plt.title(' ', fontsize=32)
+#
+plt.tight_layout()
+#plt.savefig(sim_data.home_dir+'/orbit_data/plots/mass_profiles/mass_profile_evolution_avg_all_shaded.pdf')
+#plt.savefig(sim_data.home_dir+'/orbit_data/plots/mass_profiles/mass_profile_evolution_avg_500Myr_shaded.pdf')
+#plt.savefig(sim_data.home_dir+'/orbit_data/plots/mass_profiles/mass_profile_evolution_avg_1Gyr_shaded.pdf')
+plt.savefig(sim_data.home_dir+'/orbit_data/plots/mass_profiles/mass_profile_evolution_avg_z0_shaded.pdf')
+plt.close()
