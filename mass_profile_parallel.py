73a27851d384f0bb1e48b123b90ec91bf94b8f31
Isaiah <ibsantistevan@ucdavis.edu>
2022-05-18 13:17:55 -0700

    Generating the data again, running on m12b first

--- mass_profile_parallel.py
+++ mass_profile_parallel.py
@@ -1,14 +1,14 @@
 #!/usr/bin/env python3
-#SBATCH --job-name=RR_mass_profile
+#SBATCH --job-name=m12b_mass_profile
 ##SBATCH --partition=high2    # peloton node: 32 cores, 7.8 GB per core, 250 GB total
 #SBATCH --partition=high2m    # peloton high-mem node: 32 cores, 15.6 GB per core, 500 GB total
-#SBATCH --mem=240G
+#SBATCH --mem=480G
 #SBATCH --nodes=1
 ##SBATCH --ntasks-per-node=4    # MPI tasks per node
-#SBATCH --ntasks=4    # processes total
+#SBATCH --ntasks=8    # processes total
 ##SBATCH --cpus-per-task=1    # OpenMP threads per MPI task
-#SBATCH --time=48:00:00
-#SBATCH --output=/home/ibsantis/scripts/jobs/mass_profiles/RR_mass_profile_%j.txt
+#SBATCH --time=8:00:00
+#SBATCH --output=/home/ibsantis/scripts/jobs/mass_profiles/m12b_mass_profile_%j.txt
 #SBATCH --mail-user=ibsantistevan@ucdavis.edu
 #SBATCH --mail-type=fail
 #SBATCH --mail-type=end
@@ -36,14 +36,14 @@ import orbit_io
 print('Read in the tools')
 
 ### Set path and initial parameters
-sim_data = orbit_io.OrbitRead(gal1='Romulus', location='peloton')
+sim_data = orbit_io.OrbitRead(gal1='m12b', location='peloton')
 print('Set paths')
 
 # Set up snapshot array to loop through
 #snaps = np.array([600,587,582,578,573,569,564,560,556,551,547,543,538,534,530,525,521,517,513,509,504,486,446,412,382,356,332,312,294,277,262,248,236,225,214,204,195,187,179,172,165,159,153,148,142,137,133,128,124])
 # New snapshot array just for the LG pairs
-snaps = np.array([600,587,582,578,573,569,564,560,556,551,547,544,539,534,530,525,521,517,513,509,504,486,446,412,382,356,332,312,294,277,262,248,236,225,214,204,195,187,179,172,165,159,153,148,142,137,133,128,124])
-times = np.array([13.8,13.7,13.6,13.5,13.4,13.3,13.2,13.1,13.0,12.9,12.8,12.7,12.6,12.5,12.4,12.3,12.2,12.1,12.0,11.9,11.8, 11.36, 10.37,9.51,8.73,8.06,7.43,6.90,6.43,6.00,5.60,5.24,4.94,4.66,4.38,4.14,3.92,3.73,3.54,3.37,3.21,3.07,2.93,2.82,2.69,2.58,2.49,2.38,2.29])
+snaps = np.array([600,587,582,578,573,569,564,560,556,551,547,544,539,534,530,525,521,517,513,509,504,484,463,443,424,404,385,365,346,327,308,289,270,250,231,211,190,169,147,124])
+times = np.array([13.8,13.7,13.6,13.5,13.4,13.3,13.2,13.1,13.0,12.9,12.8,12.7,12.6,12.5,12.4,12.3,12.2,12.1,12.0,11.9,11.8,11.3,10.8,10.3,9.8,9.3,8.8,8.3,7.8,7.3,6.8,6.3,5.8,5.3,4.8,4.3,3.8,3.3,2.8,2.3])
 rs = np.logspace(np.log10(0.1), np.log10(500), 100)
 
 # function from first loop down # TRY RUNNING A COUPLE SNAPSHOTS IN A LOOP FIRST TO MAKE SURE IT WORKS
@@ -101,7 +101,7 @@ args_list = [
     (snapshot, sim_data, rs) for snapshot in snaps
     ]
 
-ut.io.run_in_parallel(mass_evolution, args_list, proc_number=4) # ADD VERBOSE
+ut.io.run_in_parallel(mass_evolution, args_list, proc_number=8) # ADD VERBOSE
 # Try 4 at first, then try 8
 # How many snaps can I read in simultaneously? Divide total mem by snapshots to get proc number
 #
