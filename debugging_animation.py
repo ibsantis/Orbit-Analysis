36b9859dcabb7f97d3d0b56a7a64aea2b19a71e0
Isaiah <ibsantistevan@ucdavis.edu>
2022-06-15 13:37:32 -0700

    Adding more again

--- debugging_animation.py
+++ debugging_animation.py
@@ -36,3 +36,28 @@ traj_X = np.flip(data['d.sim'][:,:,0], axis=1)
 traj_Y = np.flip(data['d.sim'][:,:,1], axis=1)
 traj_Z = np.flip(data['d.sim'][:,:,2], axis=1)
 print('Finished setting up plotting arrays')
+
+# Display a single trajectory
+R200m = data['host.radius'][0]+10
+fig, ax = plt.subplots(1, 2, figsize=(16,8))
+ax[0].set(xlim=((-1)*R200m, R200m), ylim=((-1)*R200m, R200m))
+ax[0].set_xlabel('X [kpc]', fontsize=20)
+ax[0].set_ylabel('Y [kpc]', fontsize=20)
+#
+ax[1].set(xlim=((-1)*R200m, R200m), ylim=((-1)*R200m, R200m))
+ax[1].set_xlabel('X [kpc]', fontsize=20)
+ax[1].set_ylabel('Z [kpc]', fontsize=20)
+plt.suptitle(sim_data.galaxy+' satellites', fontsize=28)
+#
+# Set up the colors
+colorss = np.array(['#ff0000','#c71585','#40e0d0','#00ff00','#0000ff','#1e90ff'])
+#
+ax[0].plot(np.nan, np.nan, marker='o', markersize=3, markeredgecolor=colorss[0], markerfacecolor=colorss[0], alpha=0.5, label='$M_{\\rm star} < 10^5 M_{\\odot}$')
+ax[0].plot(np.nan, np.nan, marker='o', markersize=4, markeredgecolor=colorss[1], markerfacecolor=colorss[1], alpha=0.5, label='$M_{\\rm star} = [10^5,10^6] M_{\\odot}$')
+ax[0].plot(np.nan, np.nan, marker='o', markersize=5, markeredgecolor=colorss[2], markerfacecolor=colorss[2], alpha=0.5, label='$M_{\\rm star} = [10^6,10^7] M_{\\odot}$')
+ax[0].plot(np.nan, np.nan, marker='o', markersize=6, markeredgecolor=colorss[3], markerfacecolor=colorss[3], alpha=0.5, label='$M_{\\rm star} = [10^7,10^8] M_{\\odot}$')
+ax[0].plot(np.nan, np.nan, marker='o', markersize=7, markeredgecolor=colorss[4], markerfacecolor=colorss[4], alpha=0.5, label='$M_{\\rm star} = [10^8,10^9] M_{\\odot}$')
+ax[0].plot(np.nan, np.nan, marker='o', markersize=8, markeredgecolor=colorss[5], markerfacecolor=colorss[5], alpha=0.5, label='$M_{\\rm star} = [10^9,10^{10}] M_{\\odot}$')
+ax[0].plot(np.nan, np.nan, marker='o', markersize=9, markeredgecolor='k', markerfacecolor='k', alpha=0.5, label='$M_{\\rm star} > 10^{10} M_{\\odot}$')
+ax[0].legend(prop={'size': 16}, loc='best')
+print('Finished setting up axes')
