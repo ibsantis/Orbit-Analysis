2188129a104bd1fe39f579151fc2151508384c82
Isaiah <ibsantistevan@ucdavis.edu>
2022-07-05 15:14:03 -0700

    Made some tweaks to the plots I'm going to show in the paper

--- mass_profile_evolution_plot.py
+++ mass_profile_evolution_plot.py
@@ -176,9 +176,9 @@ colorss = color_cycle(len(inds), cmap_name='plasma', low=0, high=1)
 plt.rcParams["font.family"] = "serif"
 plt.figure(figsize=(10, 8))
 for i in range(0, len(inds)):
-    plt.plot(rs[1:], mass_prof_all_med[i], color=colorss[i])
+    plt.plot(rs[1:], mass_prof_all_med[inds[i]], color=colorss[i])
 plt.xlim(5, 350)
-plt.ylim(0.85, 1.03)
+plt.ylim(0, 1.2)
 plt.xscale('log')
 plt.xlabel('r [kpc]', fontsize=32)
 plt.ylabel('$M_{\\rm median}(<r)$ / $M_{\\rm z = 0, median}$(<r)', fontsize=32)
@@ -228,7 +228,7 @@ colorss = color_cycle(len(mass_m12b['time'][:21]), cmap_name='plasma', low=0, hi
 plt.rcParams["font.family"] = "serif"
 plt.figure(figsize=(10, 8))
 for i in inds:
-    plt.plot(rs[1:], mass_prof_avg_all_med[i], color=colorss[i])
+    plt.plot(rs[1:], mass_prof_avg_all_med[inds[i]], color=colorss[i])
 plt.xlim(5, 350)
 plt.ylim(0.95, 1.05)
 plt.hlines(1, 0.1, 500, color='k', alpha=0.8, linestyles='dotted', zorder=100)
@@ -244,51 +244,267 @@ plt.tight_layout()
 plt.savefig(sim_data.home_dir+'/orbit_data/plots/mass_profiles/mass_profile_evolution_2Gyr_avg_median.pdf')
 plt.close()
 
-
-
-
-
-
-##### OLD AND DONT WANT TO KEEP ANYMORE BUT TALK TO ANDREW ABOUT THIS FIRST CAUSE HE MIGHT WANT THESE
-# Sigma percents
+# Plotting the 68%, 95%, and 100% scatters across all hosts and times for each distance
 onesigp = 84.13
 onesigm = 15.87
 #
 twosigp = 97.72
 twosigm = 2.28
 #
-thrsigp = 99.87
-thrsigm = 0.13
+thrsigp = 100
+thrsigm = 0
 #
-upper_one = np.percentile((mass_prof_med_all/mass_prof_med_all[0]), onesigp, axis=0)
-lower_one = np.percentile((mass_prof_med_all/mass_prof_med_all[0]), onesigm, axis=0)
-upper_two = np.percentile((mass_prof_med_all/mass_prof_med_all[0]), twosigp, axis=0)
-lower_two = np.percentile((mass_prof_med_all/mass_prof_med_all[0]), twosigm, axis=0)
-upper_thr = np.percentile((mass_prof_med_all/mass_prof_med_all[0]), thrsigp, axis=0)
-lower_thr = np.percentile((mass_prof_med_all/mass_prof_med_all[0]), thrsigm, axis=0)
+upper_one = np.percentile(mass_prof_avg_all, onesigp, axis=(0,1))
+lower_one = np.percentile(mass_prof_avg_all, onesigm, axis=(0,1))
+upper_two = np.percentile(mass_prof_avg_all, twosigp, axis=(0,1))
+lower_two = np.percentile(mass_prof_avg_all, twosigm, axis=(0,1))
+upper_thr = np.percentile(mass_prof_avg_all, thrsigp, axis=(0,1))
+lower_thr = np.percentile(mass_prof_avg_all, thrsigm, axis=(0,1))
 
-colorss = ['#93E9BE', '#008080', '#0080FF']
+colorss = ['#93E9BE', '#008080']
 
 plt.rcParams["font.family"] = "serif"
 plt.figure(figsize=(10, 8))
-plt.fill_between(rs[1:], upper_thr, lower_thr, color='#9966cc', alpha=0.3)
+plt.fill_between(rs[1:], upper_thr, lower_thr, color='#9966cc', alpha=0.15)
 plt.fill_between(rs[1:], upper_two, lower_two, color='#9966cc', alpha=0.5)
 plt.fill_between(rs[1:], upper_one, lower_one, color='#9966cc', alpha=1)
+plt.hlines(1, 0.1, 500, color='k', alpha=0.8, linestyles='dotted', zorder=100)
 plt.xlim(5, 350)
-plt.ylim(0, 1.2)
-plt.hlines(1, 0.1, 500, color='k', alpha=1, linestyles='dotted', zorder=100)
+plt.ylim(0.75, 1.37)
 plt.xscale('log')
 plt.xlabel('r [kpc]', fontsize=32)
-#plt.ylabel('$M$(<r) / $M_{\\rm avg}$(<r)', fontsize=32)
-plt.ylabel('$M$(<r) / $M_{z=0}$(<r)', fontsize=32)
-#plt.title('Average over last 11.5 Gyr', fontsize=32)
-#plt.title('Average over last 500 Myr', fontsize=32)
-#plt.title('Average over last 1 Gyr', fontsize=32)
-plt.title(' ', fontsize=32)
-#
+plt.ylabel('$M_{\\rm median}(<r)$ / $M_{\\rm avg, 2\ Gyr}$(<r)', fontsize=32)
 plt.tight_layout()
-#plt.savefig(sim_data.home_dir+'/orbit_data/plots/mass_profiles/mass_profile_evolution_avg_all_shaded.pdf')
-#plt.savefig(sim_data.home_dir+'/orbit_data/plots/mass_profiles/mass_profile_evolution_avg_500Myr_shaded.pdf')
-#plt.savefig(sim_data.home_dir+'/orbit_data/plots/mass_profiles/mass_profile_evolution_avg_1Gyr_shaded.pdf')
-plt.savefig(sim_data.home_dir+'/orbit_data/plots/mass_profiles/mass_profile_evolution_avg_z0_shaded.pdf')
+plt.savefig(sim_data.home_dir+'/orbit_data/plots/mass_profiles/mass_profile_evolution_2Gyr_shaded.pdf')
+plt.close()
+
+
+"""
+    Plotting the enclosed mass within 100 kpc for each host at each snapshot
+"""
+
+mass_prof_all_100 = np.array([np.cumsum(mass_m12b['mass.profile'][:,:81],axis=1)/np.cumsum(mass_m12b['mass.profile'][0][:81]),\
+                          np.cumsum(mass_m12c['mass.profile'][:,:81],axis=1)/np.cumsum(mass_m12c['mass.profile'][0][:81]),\
+                          np.cumsum(mass_m12f['mass.profile'][:,:81],axis=1)/np.cumsum(mass_m12f['mass.profile'][0][:81]),\
+                          np.cumsum(mass_m12i['mass.profile'][:,:81],axis=1)/np.cumsum(mass_m12i['mass.profile'][0][:81]),\
+                          np.cumsum(mass_m12m['mass.profile'][:,:81],axis=1)/np.cumsum(mass_m12m['mass.profile'][0][:81]),\
+                          np.cumsum(mass_m12r['mass.profile'][:,:81],axis=1)/np.cumsum(mass_m12r['mass.profile'][0][:81]),\
+                          np.cumsum(mass_m12w['mass.profile'][:,:81],axis=1)/np.cumsum(mass_m12w['mass.profile'][0][:81]),\
+                          np.cumsum(mass_m12z['mass.profile'][:,:81],axis=1)/np.cumsum(mass_m12z['mass.profile'][0][:81]),\
+                          np.cumsum(mass_Romeo['mass.profile'][:,:81],axis=1)/np.cumsum(mass_Romeo['mass.profile'][0][:81]),\
+                          np.cumsum(mass_Juliet['mass.profile'][:,:81],axis=1)/np.cumsum(mass_Juliet['mass.profile'][0][:81]),\
+                          np.cumsum(mass_Thelma['mass.profile'][:,:81],axis=1)/np.cumsum(mass_Thelma['mass.profile'][0][:81]),\
+                          np.cumsum(mass_Louise['mass.profile'][:,:81],axis=1)/np.cumsum(mass_Louise['mass.profile'][0][:81]),\
+                          np.cumsum(mass_Romulus['mass.profile'][:,:81],axis=1)/np.cumsum(mass_Romulus['mass.profile'][0][:81]),\
+                          np.cumsum(mass_Remus['mass.profile'][:,:81],axis=1)/np.cumsum(mass_Remus['mass.profile'][0][:81])])
+#
+times = np.around(13.8-mass_m12b['time'], decimals=2)
+#
+plt.rcParams["font.family"] = "serif"
+plt.figure(figsize=(10, 8))
+for i in range(0, len(mass_prof_all_100)):
+    plt.plot(times, mass_prof_all_100[i][:,-1], summary_plot.colors[i], label=summary_plot.host_names['all'][i])
+plt.xlim(13, 0)
+#plt.ylim(1e11, 2e12)
+#plt.hlines(1, 0.1, 500, color='k', alpha=0.8, linestyles='dotted', zorder=100)
+plt.xscale('linear')
+plt.yscale('linear')
+plt.xlabel('Lookback time [Gyr]', fontsize=32)
+plt.ylabel('$M(z)/M(z=0)$', fontsize=32)
+plt.title('Mass enclosed within 100 kpc')
+plt.legend(ncol=3)
+plt.tight_layout()
+#plt.show()
+plt.savefig(sim_data.home_dir+'/orbit_data/plots/mass_profiles/mass_profile_evolution_100kpc_ind.pdf')
+plt.close()
+
+
+## Same as above, but showing just the scatters
+# Sigma percents
+onesigp = 84.13
+onesigm = 15.87
+#
+twosigp = 100
+twosigm = 0
+#
+upper_one = np.percentile(mass_prof_all_100, onesigp, axis=0)[:,-1]
+lower_one = np.percentile(mass_prof_all_100, onesigm, axis=0)[:,-1]
+upper_two = np.percentile(mass_prof_all_100, twosigp, axis=0)[:,-1]
+lower_two = np.percentile(mass_prof_all_100, twosigm, axis=0)[:,-1]
+#
+med = np.median(mass_prof_all_100, axis=0)
+
+colorss = ['#93E9BE', '#008080']
+
+plt.rcParams["font.family"] = "serif"
+plt.figure(figsize=(10, 8))
+plt.plot(times, med[:,-1], color='#9966cc', alpha=1)
+plt.fill_between(times, upper_two, lower_two, color='#9966cc', alpha=0.3)
+plt.fill_between(times, upper_one, lower_one, color='#9966cc', alpha=0.5)
+plt.hlines(1, 13, 0, color='k', alpha=0.8, linestyles='dotted', zorder=100)
+plt.xlim(13, 0)
+#plt.ylim(0.95, 1.05)
+plt.xscale('linear')
+plt.yscale('linear')
+plt.xlabel('Lookback time [Gyr]', fontsize=32)
+plt.ylabel('$M(z)/M(z=0)$', fontsize=32)
+plt.title('Mass enclosed within 100 kpc')
+plt.tight_layout()
+plt.savefig(sim_data.home_dir+'/orbit_data/plots/mass_profiles/mass_profile_evolution_100kpc_scatter.pdf')
+plt.close()
+
+
+
+"""
+    Plotting the enclosed mass within 50 kpc for each host at each snapshot
+"""
+
+mass_prof_all_50 = np.array([np.cumsum(mass_m12b['mass.profile'][:,:73],axis=1)/np.cumsum(mass_m12b['mass.profile'][0][:73]),\
+                          np.cumsum(mass_m12c['mass.profile'][:,:73],axis=1)/np.cumsum(mass_m12c['mass.profile'][0][:73]),\
+                          np.cumsum(mass_m12f['mass.profile'][:,:73],axis=1)/np.cumsum(mass_m12f['mass.profile'][0][:73]),\
+                          np.cumsum(mass_m12i['mass.profile'][:,:73],axis=1)/np.cumsum(mass_m12i['mass.profile'][0][:73]),\
+                          np.cumsum(mass_m12m['mass.profile'][:,:73],axis=1)/np.cumsum(mass_m12m['mass.profile'][0][:73]),\
+                          np.cumsum(mass_m12r['mass.profile'][:,:73],axis=1)/np.cumsum(mass_m12r['mass.profile'][0][:73]),\
+                          np.cumsum(mass_m12w['mass.profile'][:,:73],axis=1)/np.cumsum(mass_m12w['mass.profile'][0][:73]),\
+                          np.cumsum(mass_m12z['mass.profile'][:,:73],axis=1)/np.cumsum(mass_m12z['mass.profile'][0][:73]),\
+                          np.cumsum(mass_Romeo['mass.profile'][:,:73],axis=1)/np.cumsum(mass_Romeo['mass.profile'][0][:73]),\
+                          np.cumsum(mass_Juliet['mass.profile'][:,:73],axis=1)/np.cumsum(mass_Juliet['mass.profile'][0][:73]),\
+                          np.cumsum(mass_Thelma['mass.profile'][:,:73],axis=1)/np.cumsum(mass_Thelma['mass.profile'][0][:73]),\
+                          np.cumsum(mass_Louise['mass.profile'][:,:73],axis=1)/np.cumsum(mass_Louise['mass.profile'][0][:73]),\
+                          np.cumsum(mass_Romulus['mass.profile'][:,:73],axis=1)/np.cumsum(mass_Romulus['mass.profile'][0][:73]),\
+                          np.cumsum(mass_Remus['mass.profile'][:,:73],axis=1)/np.cumsum(mass_Remus['mass.profile'][0][:73])])
+#
+times = np.around(13.8-mass_m12b['time'], decimals=2)
+#
+plt.rcParams["font.family"] = "serif"
+plt.figure(figsize=(10, 8))
+for i in range(0, len(mass_prof_all_50)):
+    plt.plot(times, mass_prof_all_50[i][:,-1], summary_plot.colors[i], label=summary_plot.host_names['all'][i])
+plt.xlim(13, 0)
+#plt.ylim(1e11, 2e12)
+#plt.hlines(1, 0.1, 500, color='k', alpha=0.8, linestyles='dotted', zorder=100)
+plt.xscale('linear')
+plt.yscale('linear')
+plt.xlabel('Lookback time [Gyr]', fontsize=32)
+plt.ylabel('$M(z)/M(z=0)$', fontsize=32)
+plt.title('Mass enclosed within 50 kpc')
+plt.legend(ncol=3)
+plt.tight_layout()
+#plt.show()
+plt.savefig(sim_data.home_dir+'/orbit_data/plots/mass_profiles/mass_profile_evolution_50kpc_ind.pdf')
+plt.close()
+
+
+onesigp = 84.13
+onesigm = 15.87
+#
+twosigp = 100
+twosigm = 0
+#
+upper_one = np.percentile(mass_prof_all_50, onesigp, axis=0)[:,-1]
+lower_one = np.percentile(mass_prof_all_50, onesigm, axis=0)[:,-1]
+upper_two = np.percentile(mass_prof_all_50, twosigp, axis=0)[:,-1]
+lower_two = np.percentile(mass_prof_all_50, twosigm, axis=0)[:,-1]
+#
+med = np.median(mass_prof_all_50, axis=0)
+
+colorss = ['#93E9BE', '#008080']
+
+plt.rcParams["font.family"] = "serif"
+plt.figure(figsize=(10, 8))
+plt.plot(times, med[:,-1], color='#9966cc', alpha=1)
+plt.fill_between(times, upper_two, lower_two, color='#9966cc', alpha=0.3)
+plt.fill_between(times, upper_one, lower_one, color='#9966cc', alpha=0.5)
+plt.hlines(1, 13, 0, color='k', alpha=0.8, linestyles='dotted', zorder=100)
+plt.xlim(13, 0)
+#plt.ylim(0.95, 1.05)
+plt.xscale('linear')
+plt.yscale('linear')
+plt.xlabel('Lookback time [Gyr]', fontsize=32)
+plt.ylabel('$M(z)/M(z=0)$', fontsize=32)
+plt.title('Mass enclosed within 50 kpc')
+plt.tight_layout()
+plt.savefig(sim_data.home_dir+'/orbit_data/plots/mass_profiles/mass_profile_evolution_50kpc_scatter.pdf')
+plt.close()
+
+
+
+
+mass_prof_all_50 = np.array([np.cumsum(mass_m12b['mass.profile'][:,:73],axis=1)/np.cumsum(mass_m12b['mass.profile'][0][:73]),\
+                          np.cumsum(mass_m12c['mass.profile'][:,:73],axis=1)/np.cumsum(mass_m12c['mass.profile'][0][:73]),\
+                          np.cumsum(mass_m12f['mass.profile'][:,:73],axis=1)/np.cumsum(mass_m12f['mass.profile'][0][:73]),\
+                          np.cumsum(mass_m12i['mass.profile'][:,:73],axis=1)/np.cumsum(mass_m12i['mass.profile'][0][:73]),\
+                          np.cumsum(mass_m12m['mass.profile'][:,:73],axis=1)/np.cumsum(mass_m12m['mass.profile'][0][:73]),\
+                          np.cumsum(mass_m12r['mass.profile'][:,:73],axis=1)/np.cumsum(mass_m12r['mass.profile'][0][:73]),\
+                          np.cumsum(mass_m12w['mass.profile'][:,:73],axis=1)/np.cumsum(mass_m12w['mass.profile'][0][:73]),\
+                          np.cumsum(mass_m12z['mass.profile'][:,:73],axis=1)/np.cumsum(mass_m12z['mass.profile'][0][:73]),\
+                          np.cumsum(mass_Romeo['mass.profile'][:,:73],axis=1)/np.cumsum(mass_Romeo['mass.profile'][0][:73]),\
+                          np.cumsum(mass_Juliet['mass.profile'][:,:73],axis=1)/np.cumsum(mass_Juliet['mass.profile'][0][:73]),\
+                          np.cumsum(mass_Thelma['mass.profile'][:,:73],axis=1)/np.cumsum(mass_Thelma['mass.profile'][0][:73]),\
+                          np.cumsum(mass_Louise['mass.profile'][:,:73],axis=1)/np.cumsum(mass_Louise['mass.profile'][0][:73]),\
+                          np.cumsum(mass_Romulus['mass.profile'][:,:73],axis=1)/np.cumsum(mass_Romulus['mass.profile'][0][:73]),\
+                          np.cumsum(mass_Remus['mass.profile'][:,:73],axis=1)/np.cumsum(mass_Remus['mass.profile'][0][:73])])
+
+
+mass_prof_all_100 = np.array([np.cumsum(mass_m12b['mass.profile'][:,:81],axis=1)/np.cumsum(mass_m12b['mass.profile'][0][:81]),\
+                          np.cumsum(mass_m12c['mass.profile'][:,:81],axis=1)/np.cumsum(mass_m12c['mass.profile'][0][:81]),\
+                          np.cumsum(mass_m12f['mass.profile'][:,:81],axis=1)/np.cumsum(mass_m12f['mass.profile'][0][:81]),\
+                          np.cumsum(mass_m12i['mass.profile'][:,:81],axis=1)/np.cumsum(mass_m12i['mass.profile'][0][:81]),\
+                          np.cumsum(mass_m12m['mass.profile'][:,:81],axis=1)/np.cumsum(mass_m12m['mass.profile'][0][:81]),\
+                          np.cumsum(mass_m12r['mass.profile'][:,:81],axis=1)/np.cumsum(mass_m12r['mass.profile'][0][:81]),\
+                          np.cumsum(mass_m12w['mass.profile'][:,:81],axis=1)/np.cumsum(mass_m12w['mass.profile'][0][:81]),\
+                          np.cumsum(mass_m12z['mass.profile'][:,:81],axis=1)/np.cumsum(mass_m12z['mass.profile'][0][:81]),\
+                          np.cumsum(mass_Romeo['mass.profile'][:,:81],axis=1)/np.cumsum(mass_Romeo['mass.profile'][0][:81]),\
+                          np.cumsum(mass_Juliet['mass.profile'][:,:81],axis=1)/np.cumsum(mass_Juliet['mass.profile'][0][:81]),\
+                          np.cumsum(mass_Thelma['mass.profile'][:,:81],axis=1)/np.cumsum(mass_Thelma['mass.profile'][0][:81]),\
+                          np.cumsum(mass_Louise['mass.profile'][:,:81],axis=1)/np.cumsum(mass_Louise['mass.profile'][0][:81]),\
+                          np.cumsum(mass_Romulus['mass.profile'][:,:81],axis=1)/np.cumsum(mass_Romulus['mass.profile'][0][:81]),\
+                          np.cumsum(mass_Remus['mass.profile'][:,:81],axis=1)/np.cumsum(mass_Remus['mass.profile'][0][:81])])
+
+mass_prof_all_200 = np.array([np.cumsum(mass_m12b['mass.profile'][:,:89],axis=1)/np.cumsum(mass_m12b['mass.profile'][0][:89]),\
+                          np.cumsum(mass_m12c['mass.profile'][:,:89],axis=1)/np.cumsum(mass_m12c['mass.profile'][0][:89]),\
+                          np.cumsum(mass_m12f['mass.profile'][:,:89],axis=1)/np.cumsum(mass_m12f['mass.profile'][0][:89]),\
+                          np.cumsum(mass_m12i['mass.profile'][:,:89],axis=1)/np.cumsum(mass_m12i['mass.profile'][0][:89]),\
+                          np.cumsum(mass_m12m['mass.profile'][:,:89],axis=1)/np.cumsum(mass_m12m['mass.profile'][0][:89]),\
+                          np.cumsum(mass_m12r['mass.profile'][:,:89],axis=1)/np.cumsum(mass_m12r['mass.profile'][0][:89]),\
+                          np.cumsum(mass_m12w['mass.profile'][:,:89],axis=1)/np.cumsum(mass_m12w['mass.profile'][0][:89]),\
+                          np.cumsum(mass_m12z['mass.profile'][:,:89],axis=1)/np.cumsum(mass_m12z['mass.profile'][0][:89]),\
+                          np.cumsum(mass_Romeo['mass.profile'][:,:89],axis=1)/np.cumsum(mass_Romeo['mass.profile'][0][:89]),\
+                          np.cumsum(mass_Juliet['mass.profile'][:,:89],axis=1)/np.cumsum(mass_Juliet['mass.profile'][0][:89]),\
+                          np.cumsum(mass_Thelma['mass.profile'][:,:89],axis=1)/np.cumsum(mass_Thelma['mass.profile'][0][:89]),\
+                          np.cumsum(mass_Louise['mass.profile'][:,:89],axis=1)/np.cumsum(mass_Louise['mass.profile'][0][:89]),\
+                          np.cumsum(mass_Romulus['mass.profile'][:,:89],axis=1)/np.cumsum(mass_Romulus['mass.profile'][0][:89]),\
+                          np.cumsum(mass_Remus['mass.profile'][:,:89],axis=1)/np.cumsum(mass_Remus['mass.profile'][0][:89])])
+
+
+
+upper_one_50 = np.percentile(mass_prof_all_50, onesigp, axis=0)[:,-1]
+lower_one_50 = np.percentile(mass_prof_all_50, onesigm, axis=0)[:,-1]
+upper_two_50 = np.percentile(mass_prof_all_50, twosigp, axis=0)[:,-1]
+lower_two_50 = np.percentile(mass_prof_all_50, twosigm, axis=0)[:,-1]
+#
+med_50 = np.median(mass_prof_all_50, axis=0)
+med_100 = np.median(mass_prof_all_100, axis=0)
+med_200 = np.median(mass_prof_all_200, axis=0)
+
+colorss = ['#93E9BE', '#008080']
+times = np.around(13.8-mass_m12b['time'], decimals=2)
+#
+plt.rcParams["font.family"] = "serif"
+plt.figure(figsize=(10, 8))
+plt.plot(times, med_50[:,-1], color=summary_plot.colors[1], alpha=1, label='50 kpc')
+plt.fill_between(times, upper_two_50, lower_two_50, color=summary_plot.colors[1], alpha=0.15)
+plt.fill_between(times, upper_one_50, lower_one_50, color=summary_plot.colors[1], alpha=0.3)
+plt.plot(times, med_100[:,-1], color=summary_plot.colors[3], alpha=1, label='100 kpc')
+plt.plot(times, med_200[:,-1], color=summary_plot.colors[6], alpha=1, label='200 kpc')
+plt.hlines(1, 13, 0, color='k', alpha=0.8, linestyles='dotted', zorder=100)
+plt.xlim(13, 0)
+#plt.ylim(0.95, 1.05)
+plt.xscale('linear')
+plt.yscale('linear')
+plt.xlabel('Lookback time [Gyr]', fontsize=32)
+plt.ylabel('$M(<d, z)$ / $M(<d, z=0)$', fontsize=32)
+plt.legend(prop={'size': 24}, loc='best')
+plt.tight_layout()
+plt.savefig(sim_data.home_dir+'/orbit_data/plots/mass_profiles/mass_profile_evolution_enclosed.pdf')
 plt.close()
