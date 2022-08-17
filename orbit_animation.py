88664b1cd1deb4b9d0a04642e1140f7227a73145
Isaiah <ibsantistevan@ucdavis.edu>
2022-08-17 08:55:31 -0700

    Looking at possible interaction for sat in Thelma

--- orbit_animation.py
+++ orbit_animation.py
@@ -19,7 +19,7 @@ print('Read in the tools')
 sim_data = orbit_io.OrbitRead(gal1='m12i', location='mac')
 print('Set paths')
 
-data = ut.io.file_hdf5(file_name_base=sim_data.home_dir+'/orbit_data/hdf5_files/summary_data/data_Romulus')
+data = ut.io.file_hdf5(file_name_base=sim_data.home_dir+'/orbit_data/hdf5_files/summary_data/data_Thelma')
 
 colors = ['#2f4f4f', '#006400', '#8b0000', '#000080', '#00ced1',\
           '#ff8c00', '#c71585', '#7fff00', '#00fa9a', '#0000ff',\
@@ -387,3 +387,61 @@ plt.tight_layout()
 writergif = animation.PillowWriter(fps=30)
 anim.save('/Users/isaiahsantistevan/Desktop/duo_Romulus.gif',writer=writergif)
 #plt.show()
+
+
+
+
+
+
+
+# EXAMPLE 6 Plotting possible subhalos orbiting each other in Thelma
+# Multiple halos
+fig = plt.figure()
+ax = plt.axes(projection='3d')
+sub_ind = 117
+mask_real = (data['d.sim'][100][:,0] != -1)
+mask_test = (data['d.sim'][sub_ind][:,0] != -1)
+#
+ax.set_xlim(np.min(np.concatenate((data['d.sim'][100][:,0][mask_real],data['d.sim'][sub_ind][:,0][mask_test]))), np.max(np.concatenate((data['d.sim'][100][:,0][mask_real],data['d.sim'][sub_ind][:,0][mask_test]))))
+ax.set_ylim(np.min(np.concatenate((data['d.sim'][100][:,1][mask_real],data['d.sim'][sub_ind][:,1][mask_test]))), np.max(np.concatenate((data['d.sim'][100][:,1][mask_real],data['d.sim'][sub_ind][:,1][mask_test]))))
+ax.set_zlim(np.min(np.concatenate((data['d.sim'][100][:,2][mask_real],data['d.sim'][sub_ind][:,2][mask_test]))), np.max(np.concatenate((data['d.sim'][100][:,2][mask_real],data['d.sim'][sub_ind][:,2][mask_test]))))
+#
+ax.set_xlabel('X [kpc]', labelpad=15.)
+ax.set_ylabel('Y [kpc]', labelpad=15.)
+ax.set_zlabel('Z [kpc]', labelpad=15.)
+#
+line_real, = ax.plot3D(np.array([]),np.array([]),np.array([]), colors[0])
+line_test, = ax.plot3D(np.array([]),np.array([]),np.array([]), colors[1])
+ax.scatter(0,0,0,color='black')
+
+def init():
+    line_real.set_data(np.array([]),np.array([]))
+    line_real.set_3d_properties(np.array([]))
+    line_test.set_data(np.array([]),np.array([]))
+    line_test.set_3d_properties(np.array([]))
+    return line_real, line_test,
+
+def animate(i):
+    x_real = data['d.sim'][100][:,0][mask_real][:i]
+    y_real = data['d.sim'][100][:,1][mask_real][:i]
+    z_real = data['d.sim'][100][:,2][mask_real][:i]
+    #
+    x_test = data['d.sim'][sub_ind][:,0][mask_test][:i]
+    y_test = data['d.sim'][sub_ind][:,1][mask_test][:i]
+    z_test = data['d.sim'][sub_ind][:,2][mask_test][:i]
+    #
+    line_real.set_data(x_real,y_real)
+    line_real.set_3d_properties(z_real)
+    #
+    line_test.set_data(x_test,y_test)
+    line_test.set_3d_properties(z_test)
+    #
+    ax.view_init(elev=30, azim=0.6*i)
+    #
+    return line_real, line_test,
+
+anim = animation.FuncAnimation(fig, animate, init_func=init, frames=len(data['d.sim'][100][:,0][mask_real]), interval=0.1, blit=False)
+plt.tight_layout()
+#writergif = animation.PillowWriter(fps=30)
+#anim.save('/Users/isaiahsantistevan/Desktop/duo_Romulus.gif',writer=writergif)
+plt.show()
