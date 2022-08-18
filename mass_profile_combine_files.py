ff4c5c666e5f948eb110d3ad063c262c2532af02
Isaiah <ibsantistevan@ucdavis.edu>
2022-08-18 15:27:40 -0700

    Running this on all the data from m12i

--- mass_profile_combine_files.py
+++ mass_profile_combine_files.py
@@ -21,32 +21,42 @@ print('Read in the tools')
 ## FOR ISOLATED HOSTS
 #
 ### Set path and initial parameters
-sim_data = orbit_io.OrbitRead(gal1=str(sys.argv[1]), location='mac')
+sim_data = orbit_io.OrbitRead(gal1=str(sys.argv[1]), location='peloton')
 print('Set paths')
 
 # Set up snapshot array to loop through
-snaps = np.array([600,587,582,578,573,569,564,560,556,551,547,544,539,534,530,525,521,517,513,509,504,484,463,443,424,404,385,365,346,327,308,289,270,250,231,211,190,169,147,124,99,72,42])
-times = np.array([13.8,13.7,13.6,13.5,13.4,13.3,13.2,13.1,13.0,12.9,12.8,12.7,12.6,12.5,12.4,12.3,12.2,12.1,12.0,11.9,11.8,11.3,10.8,10.3,9.8,9.3,8.8,8.3,7.8,7.3,6.8,6.3,5.8,5.3,4.8,4.3,3.8,3.3,2.8,2.3,1.8,1.3,0.8])
-rs = np.logspace(np.log10(0.1), np.log10(500), 100)
+#snaps = np.array([600,587,582,578,573,569,564,560,556,551,547,544,539,534,530,525,521,517,513,509,504,484,463,443,424,404,385,365,346,327,308,289,270,250,231,211,190,169,147,124,99,72,42])
+#times = np.array([13.8,13.7,13.6,13.5,13.4,13.3,13.2,13.1,13.0,12.9,12.8,12.7,12.6,12.5,12.4,12.3,12.2,12.1,12.0,11.9,11.8,11.3,10.8,10.3,9.8,9.3,8.8,8.3,7.8,7.3,6.8,6.3,5.8,5.3,4.8,4.3,3.8,3.3,2.8,2.3,1.8,1.3,0.8])
+#rs = np.logspace(np.log10(0.1), np.log10(500), 100)
+snaps = ut.simulation.read_snapshot_times(directory=sim_data.simulation_dir)
+snaps = snaps['index']
+times = snaps['time']
+rs = np.logspace(np.log10(5), np.log10(500), 25)
+
 
 if sim_data.num_gal == 1:
     mass_array = np.zeros((len(snaps),len(rs)-1))
 
     for i in range(0, len(snaps)):
-        data = ut.io.file_hdf5(sim_data.home_dir+'/orbit_data/hdf5_files/mass_profiles/individual/'+sim_data.galaxy+'/'+sim_data.galaxy+'_mass_profile_evolution_'+str(snaps[i]), verbose=True)
+        data = ut.io.file_hdf5(sim_data.home_dir+'/orbit_data/hdf5_files/mass_profiles/all_snapshots/'+sim_data.galaxy+'/'+sim_data.galaxy+'_mass_profile_'+str(snaps[i]), verbose=True)
         mass_array[i] = data['array']
 
     data_dict = dict()
     data_dict['mass.profile'] = mass_array
     data_dict['snapshot'] = snaps
     data_dict['time'] = times
-    ut.io.file_hdf5(file_name_base=sim_data.home_dir+'/orbit_data/hdf5_files/mass_profiles/'+sim_data.galaxy+'_full_mass_profile', dict_or_array_to_write=data_dict, verbose=True)
+    ut.io.file_hdf5(file_name_base=sim_data.home_dir+'/orbit_data/hdf5_files/mass_profiles/all_snapshots/'+sim_data.galaxy+'_mass_profile_all', dict_or_array_to_write=data_dict, verbose=True)
 
 
 
 ## FOR LG PAIRS
 #
 ### Set path and initial parameters
+# Would have to do this on Stampede...
+#
+#
+#
+#
 if sim_data.num_gal == 2:
     sim_data.galaxy = str(sys.argv[2])
     print('Set paths')
