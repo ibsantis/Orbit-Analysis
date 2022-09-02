dddc637b68fb19fc3d6a7c29b0e34dac8f8df497
Isaiah <ibsantistevan@ucdavis.edu>
2022-09-02 15:58:45 -0700

    Running on R&J

--- summary_data.py
+++ summary_data.py
@@ -1,13 +1,13 @@
 #!/usr/bin/env python3
-#SBATCH --job-name=m12z_summary_data
-##SBATCH --partition=high2m    # peloton high-mem node: 32 cores, 15.6 GB per core, 500 GB total
-#SBATCH --partition=high2    # peloton high-mem node: 32 cores, 15.6 GB per core, 500 GB total
-#SBATCH --mem=250G
-##SBATCH --mem=480G
+#SBATCH --job-name=RJ_summary_data
+#SBATCH --partition=high2m    # peloton high-mem node: 32 cores, 15.6 GB per core, 500 GB total
+##SBATCH --partition=high2    # peloton high-mem node: 32 cores, 15.6 GB per core, 500 GB total
+##SBATCH --mem=250G
+#SBATCH --mem=480G
 #SBATCH --nodes=1
 #SBATCH --ntasks=4    # processes total
-#SBATCH --time=02:00:00
-#SBATCH --output=/home/ibsantis/scripts/jobs/summary/m12z_summary_data_%j.txt
+#SBATCH --time=06:00:00
+#SBATCH --output=/home/ibsantis/scripts/jobs/summary/RJ_summary_data_%j.txt
 #SBATCH --mail-user=ibsantistevan@ucdavis.edu
 #SBATCH --mail-type=fail
 #SBATCH --mail-type=end
@@ -42,7 +42,7 @@ import pandas as pd
 print('Read in the tools')
 
 ### Set path and initial parameters
-sim_data = orbit_io.OrbitRead(gal1='m12z', location='peloton')
+sim_data = orbit_io.OrbitRead(gal1='Romeo', location='peloton')
 plotting = False
 print('Set paths')
 
