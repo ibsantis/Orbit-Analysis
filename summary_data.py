0a7366a513858f4dccfd46cb45d31fb1bdd36db7
Isaiah <ibsantistevan@ucdavis.edu>
2022-09-15 11:19:41 -0700

    Running on m12m

--- summary_data.py
+++ summary_data.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-#SBATCH --job-name=m12i_summary_data
+#SBATCH --job-name=m12m_summary_data
 #SBATCH --partition=high2m    # peloton high-mem node: 32 cores, 15.6 GB per core, 500 GB total
 ##SBATCH --partition=high2    # peloton high-mem node: 32 cores, 15.6 GB per core, 500 GB total
 ##SBATCH --mem=250G
@@ -7,7 +7,7 @@
 #SBATCH --nodes=1
 #SBATCH --ntasks=4    # processes total
 #SBATCH --time=01:00:00
-#SBATCH --output=/home/ibsantis/scripts/jobs/summary/m12i_summary_data_%j.txt
+#SBATCH --output=/home/ibsantis/scripts/jobs/summary/m12m_summary_data_%j.txt
 #SBATCH --mail-user=ibsantistevan@ucdavis.edu
 #SBATCH --mail-type=fail
 #SBATCH --mail-type=end
@@ -43,7 +43,7 @@ print('Read in the tools')
 
 ### Set path and initial parameters
 loc = 'peloton'
-sim_data = orbit_io.OrbitRead(gal1='m12i', location=loc)
+sim_data = orbit_io.OrbitRead(gal1='m12m', location=loc)
 plotting = False
 print('Set paths')
 
