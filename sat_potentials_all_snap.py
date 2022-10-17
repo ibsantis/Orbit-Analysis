d843bc2659b4c4e6195bc9da6829712c5cd9532f
Isaiah <ibsantistevan@ucdavis.edu>
2022-10-17 12:25:27 -0700

    Testing full script on 10 snapshots for m12i

--- sat_potentials_all_snap.py
+++ sat_potentials_all_snap.py
@@ -77,76 +77,76 @@ def calc_sub_potential(snap, simdata, orbit_class):
         print('Particles at snapshot {0} read in in {1} seconds'.format(snap, end-start))
         #
         # # Set up the halo inds and KDTree
-        # start = time.time()
-        # orbit_tree = orbit_io.OrbitTree(tree=halt, gal1=simdata.galaxy, location=loc, host=1, particles=part, subsampling=15)
-        # end = time.time()
-        # print('KDTree created in {0} seconds'.format(end-start))
-        # #
-        # # Create a binning scheme and mass bin array to loop over
-        # def mass_binning(mass_array, mass_range):
-        #     mass_array = np.log10(mass_array)
-        #     mask = (mass_array > mass_range[0])*(mass_array < mass_range[1])
-        #     return mask
-        # #
-        # halo_mass_bins = np.array([7., 7.5, 8., 8.5, 9., 9.5, 10., 10.5, 11., 11.5, 12.])
-        # #
-        # # Create a dictionary to save the data to
-        # data_dict = dict()
-        # data_dict['mass.bin'] = halo_mass_bins
-        # data_dict['halo.inds'] = (-1)*np.ones(len(orbit_class.sub_inds[:,600-snap]), dtype=int)
-        # data_dict['subhalo.potential'] = (-1)*np.ones(len(orbit_class.sub_inds[:,600-snap]))
-        # data_dict['particle.num'] = (-1)*np.ones(len(orbit_class.sub_inds[:,600-snap]), dtype=int)
-        # temp = np.arange(len(orbit_class.sub_inds[:,600-snap]))
-        # print('Set up a null dictionary for the data at snapshot {0}'.format(snap))
-        # #
-        # if snap == 600:
-        #     # Find the potential of the host within R200
-        #     ndist, nind = orbit_tree.neighbors(centers=halt['position'][halt['host.index'][0]], neigh_num_max=1e8, neigh_dist_max=halt['radius'][halt['host.index'][0]], workerss=4)
-        #     part_mask = (ndist[np.isfinite(ndist)] < (halt['radius'][halt['host.index'][0]]+5))*(ndist[np.isfinite(ndist)] > (halt['radius'][halt['host.index'][0]]-5))
-        #     data_dict['host.potential.R200m'] = np.mean(part['dark']['potential'][::orbit_tree.subsampling][nind[np.isfinite(ndist)][part_mask]])
-        #     data_dict['host.particle.num'] = np.sum(part_mask)
-        #     G = 6.67*10**(-11)*(1.988*10**(30))/((1000**2)*(3.086*10**(19)))
-        #     data_dict['KE.at.Rvir'] = 0.5*G*halt['mass'][halt['host.index'][0]]/halt['radius'][halt['host.index'][0]]
-        #     #
-        #     # Find the potential of the host at 100 kpc
-        #     ndist, nind = orbit_tree.neighbors(centers=halt['position'][halt['host.index'][0]], neigh_num_max=1e8, neigh_dist_max=100, workerss=4)
-        #     part_mask = (ndist[np.isfinite(ndist)] < (100+5))*(ndist[np.isfinite(ndist)] > (100-5))
-        #     data_dict['host.potential.100kpc'] = np.mean(part['dark']['potential'][::orbit_tree.subsampling][nind[np.isfinite(ndist)][part_mask]])
-        #     #
-        #     print('Finished calculating host data for snapshot 600')
-        # #
-        # # Loop over each mass bin
-        # print('Starting loop over the mass bins')
-        # for i in range(0, len(halo_mass_bins)-1):
-        #     start = time.time()
-        #     # Get the halos in a mass bin
-        #     real_halos = (orbit_class.sub_inds[:,600-snap] >= 0)
-        #     mass_mask = mass_binning(halt.prop('mass.peak', orbit_class.sub_inds[:,600-snap][real_halos]), (halo_mass_bins[i], halo_mass_bins[i+1]))
-        #     data_dict['halo.inds'][real_halos][mass_mask] = orbit_class.sub_inds[:,600-snap][real_halos][mass_mask]
-        #     #
-        #     # If there are halos, continue
-        #     if np.sum(mass_mask) != 0:
-        #         # Get the halo positions and max halo radius in the mass bin
-        #         halo_pos = halt['position'][orbit_class.sub_inds[:,600-snap][real_halos][mass_mask]]
-        #         dmax = np.around(np.max(halt['radius'][orbit_class.sub_inds[:,600-snap][real_halos][mass_mask]]))
-        #         #
-        #         # Query the particle tree and save the distances and indices
-        #         ndist, nind = orbit_tree.neighbors(centers=halo_pos, neigh_num_max=1e6, neigh_dist_max=dmax, workerss=4)
-        #         #
-        #         # Loop over the number of halos
-        #         for j in range(0, len(halo_pos)):
-        #             # Find the particles within +/- 5 kpc of the halo radius, then save the potential and particle number
-        #             part_mask = (ndist[j][np.isfinite(ndist[j])] < (halt['radius'][orbit_class.sub_inds[:,600-snap][real_halos][mass_mask]][j]+5))*(ndist[j][np.isfinite(ndist[j])] > (halt['radius'][orbit_class.sub_inds[:,600-snap][real_halos][mass_mask]][j]-5))
-        #             data_dict['subhalo.potential'][temp[mass_mask][j]] = np.mean(part['dark']['potential'][::orbit_tree.subsampling][nind[j][np.isfinite(ndist[j])][part_mask]])
-        #             data_dict['particle.num'][temp[real_halos][mass_mask][j]] = np.sum(part_mask)
-        #     #
-        #     # If no halos, say so
-        #     else:
-        #         print('No halos between {0} and {1}'.format(halo_mass_bins[i],halo_mass_bins[i+1]))
-        #     end = time.time()
-        #     print('Done with mass bin {0} in {1} seconds'.format(i, end-start))
-        # #
-        # ut.io.file_hdf5(file_name_base=simdata.home_dir+'/orbit_data/hdf5_files/potentials/all_snapshots/'+simdata.galaxy+'/'+simdata.galaxy+'_potentials_'+snap, dict_or_array_to_write=data_dict, verbose=True)
+        start = time.time()
+        orbit_tree = orbit_io.OrbitTree(tree=halt, gal1=simdata.galaxy, location=loc, host=1, particles=part, subsampling=15)
+        end = time.time()
+        print('KDTree created in {0} seconds'.format(end-start))
+        #
+        # Create a binning scheme and mass bin array to loop over
+        def mass_binning(mass_array, mass_range):
+            mass_array = np.log10(mass_array)
+            mask = (mass_array > mass_range[0])*(mass_array < mass_range[1])
+            return mask
+        #
+        halo_mass_bins = np.array([7., 7.5, 8., 8.5, 9., 9.5, 10., 10.5, 11., 11.5, 12.])
+        #
+        # Create a dictionary to save the data to
+        data_dict = dict()
+        data_dict['mass.bin'] = halo_mass_bins
+        data_dict['halo.inds'] = (-1)*np.ones(len(orbit_class.sub_inds[:,600-snap]), dtype=int)
+        data_dict['subhalo.potential'] = (-1)*np.ones(len(orbit_class.sub_inds[:,600-snap]))
+        data_dict['particle.num'] = (-1)*np.ones(len(orbit_class.sub_inds[:,600-snap]), dtype=int)
+        temp = np.arange(len(orbit_class.sub_inds[:,600-snap]))
+        print('Set up a null dictionary for the data at snapshot {0}'.format(snap))
+        #
+        if snap == 600:
+            # Find the potential of the host within R200
+            ndist, nind = orbit_tree.neighbors(centers=halt['position'][halt['host.index'][0]], neigh_num_max=1e8, neigh_dist_max=halt['radius'][halt['host.index'][0]], workerss=4)
+            part_mask = (ndist[np.isfinite(ndist)] < (halt['radius'][halt['host.index'][0]]+5))*(ndist[np.isfinite(ndist)] > (halt['radius'][halt['host.index'][0]]-5))
+            data_dict['host.potential.R200m'] = np.mean(part['dark']['potential'][::orbit_tree.subsampling][nind[np.isfinite(ndist)][part_mask]])
+            data_dict['host.particle.num'] = np.sum(part_mask)
+            G = 6.67*10**(-11)*(1.988*10**(30))/((1000**2)*(3.086*10**(19)))
+            data_dict['KE.at.Rvir'] = 0.5*G*halt['mass'][halt['host.index'][0]]/halt['radius'][halt['host.index'][0]]
+            #
+            # Find the potential of the host at 100 kpc
+            ndist, nind = orbit_tree.neighbors(centers=halt['position'][halt['host.index'][0]], neigh_num_max=1e8, neigh_dist_max=100, workerss=4)
+            part_mask = (ndist[np.isfinite(ndist)] < (100+5))*(ndist[np.isfinite(ndist)] > (100-5))
+            data_dict['host.potential.100kpc'] = np.mean(part['dark']['potential'][::orbit_tree.subsampling][nind[np.isfinite(ndist)][part_mask]])
+            #
+            print('Finished calculating host data for snapshot 600')
+        #
+        # Loop over each mass bin
+        print('Starting loop over the mass bins')
+        for i in range(0, len(halo_mass_bins)-1):
+            start = time.time()
+            # Get the halos in a mass bin
+            real_halos = (orbit_class.sub_inds[:,600-snap] >= 0)
+            mass_mask = mass_binning(halt.prop('mass.peak', orbit_class.sub_inds[:,600-snap][real_halos]), (halo_mass_bins[i], halo_mass_bins[i+1]))
+            data_dict['halo.inds'][real_halos][mass_mask] = orbit_class.sub_inds[:,600-snap][real_halos][mass_mask]
+            #
+            # If there are halos, continue
+            if np.sum(mass_mask) != 0:
+                # Get the halo positions and max halo radius in the mass bin
+                halo_pos = halt['position'][orbit_class.sub_inds[:,600-snap][real_halos][mass_mask]]
+                dmax = np.around(np.max(halt['radius'][orbit_class.sub_inds[:,600-snap][real_halos][mass_mask]]))
+                #
+                # Query the particle tree and save the distances and indices
+                ndist, nind = orbit_tree.neighbors(centers=halo_pos, neigh_num_max=1e6, neigh_dist_max=dmax, workerss=4)
+                #
+                # Loop over the number of halos
+                for j in range(0, len(halo_pos)):
+                    # Find the particles within +/- 5 kpc of the halo radius, then save the potential and particle number
+                    part_mask = (ndist[j][np.isfinite(ndist[j])] < (halt['radius'][orbit_class.sub_inds[:,600-snap][real_halos][mass_mask]][j]+5))*(ndist[j][np.isfinite(ndist[j])] > (halt['radius'][orbit_class.sub_inds[:,600-snap][real_halos][mass_mask]][j]-5))
+                    data_dict['subhalo.potential'][temp[mass_mask][j]] = np.mean(part['dark']['potential'][::orbit_tree.subsampling][nind[j][np.isfinite(ndist[j])][part_mask]])
+                    data_dict['particle.num'][temp[real_halos][mass_mask][j]] = np.sum(part_mask)
+            #
+            # If no halos, say so
+            else:
+                print('No halos between {0} and {1}'.format(halo_mass_bins[i],halo_mass_bins[i+1]))
+            end = time.time()
+            print('Done with mass bin {0} in {1} seconds'.format(i, end-start))
+        #
+        ut.io.file_hdf5(file_name_base=simdata.home_dir+'/orbit_data/hdf5_files/potentials/all_snapshots/'+simdata.galaxy+'/'+simdata.galaxy+'_potentials_'+snap, dict_or_array_to_write=data_dict, verbose=True)
 
 print('Setting up arguments')
 # Create an array of arguments for the function above
