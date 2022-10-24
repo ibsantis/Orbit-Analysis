c16d28778aab509cb54809af34da5c7eaf74bf6e
Isaiah <ibsantistevan@ucdavis.edu>
2022-10-24 10:54:50 -0700

    Ran out of memory, using less tasks

--- sat_potentials_all_snap.py
+++ sat_potentials_all_snap.py
@@ -4,8 +4,8 @@
 #SBATCH --partition=skx-normal
 ##SBATCH --mem=500G
 #SBATCH --nodes=1
-##SBATCH --ntasks=3    # processes total
-#SBATCH --tasks-per-node=3    # MPI tasks per node
+##SBATCH --ntasks=2    # processes total
+#SBATCH --tasks-per-node=2    # MPI tasks per node
 #SBATCH --cpus-per-task=1    # OpenMP threads per MPI task
 #SBATCH --time=12:00:00
 ##SBATCH --output=/home/ibsantis/scripts/jobs/potentials/all_snapshots/RJ_subhalo_potential_all_snaps_%j.txt
@@ -287,6 +287,6 @@ args_list = [(snapshot, sim_data, orbits) for snapshot in snaps]
 
 print('Starting to run on data')
 # Run the function using the arguments above in parallel
-ut.io.run_in_parallel(calc_sub_potential, args_list, proc_number=3, verbose=True) # ADD VERBOSE
+ut.io.run_in_parallel(calc_sub_potential, args_list, proc_number=2, verbose=True) # ADD VERBOSE
 
 print('All done.')
