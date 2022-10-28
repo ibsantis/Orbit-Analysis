bed3f76557339d7c02f36c8eca6d989dae6b323a
Isaiah <ibsantistevan@ucdavis.edu>
2022-10-28 14:50:07 -0700

    Script for combining the files generated from sat_potentials_all_snap.py

---
+++ combine_potential_files.py
@@ -0,0 +1,51 @@
+#!/usr/bin/python3
+
+"""
+    ===========================
+    = Potential combine files =
+    ===========================
+
+    Combine all of the files into one file for each host galaxy
+
+"""
+
+import halo_analysis as halo
+import gizmo_analysis as gizmo
+import utilities as ut
+import numpy as np
+import orbit_io
+import sys
+print('Read in the tools')
+
+
+### Set path and initial parameters
+sim_data = orbit_io.OrbitRead(gal1='m12i', location='mac')
+print('Set paths')
+
+# Set up snapshot array to loop through
+snaps = ut.simulation.read_snapshot_times(directory=sim_data.simulation_dir)
+snaps = snaps['index'][4:]
+
+if sim_data.num_gal == 1:
+    #
+    # Read in the z = 0 data so that I can get the length of the arrays
+    data_z0 = ut.io.file_hdf5(sim_data.home_dir+'/orbit_data/hdf5_files/potentials/all_snapshots/'+sim_data.galaxy+'/'+sim_data.galaxy+'_potentials_600', verbose=True)
+    #
+    data_dict = dict()
+    data_dict['snapshot'] = snaps
+    data_dict['KE.at.Rvir'] = data_z0['KE.at.Rvir']
+    data_dict['host.pot.100kpc'] = np.zeros(len(snaps))
+    data_dict['host.pot.R200m'] = data_z0['host.potential.R200m']
+    data_dict['subhalo.inds'] = np.zeros((len(data_z0['halo.inds']), len(snaps)))
+    data_dict['subhalo.pot'] = np.zeros((len(data_z0['halo.inds']), len(snaps)))
+    #
+    for i in range(0, len(snaps)):
+        data = ut.io.file_hdf5(sim_data.home_dir+'/orbit_data/hdf5_files/potentials/all_snapshots/'+sim_data.galaxy+'/'+sim_data.galaxy+'_potentials_'+str(snaps[i]), verbose=True)
+        data_dict['subhalo.inds'][:,i] = data['halo.inds']
+        data_dict['subhalo.pot'][:,i] = data['subhalo.potential']
+        if 'host.potential.100kpc' in data.keys():
+            data_dict['host.pot.100kpc'][i] = data['host.potential.100kpc']
+        else:
+            data_dict['host.pot.100kpc'][i] = np.nan
+    #
+    ut.io.file_hdf5(file_name_base=sim_data.home_dir+'/orbit_data/hdf5_files/potentials/all_snapshots/'+sim_data.galaxy+'_potentials_all', dict_or_array_to_write=data_dict, verbose=True)
