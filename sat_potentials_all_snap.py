cc261ce44cb758d70bdabc7424eceb2be99f48f5
Isaiah <ibsantistevan@ucdavis.edu>
2022-11-04 08:52:37 -0700

    I don't need as much time

--- sat_potentials_all_snap.py
+++ sat_potentials_all_snap.py
@@ -7,7 +7,7 @@
 ##SBATCH --ntasks=3    # processes total
 #SBATCH --tasks-per-node=1    # MPI tasks per node
 ##SBATCH --cpus-per-task=1    # OpenMP threads per MPI task
-#SBATCH --time=06:00:00
+#SBATCH --time=02:00:00
 ##SBATCH --output=/home/ibsantis/scripts/jobs/potentials/all_snapshots/m12w_subhalo_potential_all_snaps_%j.txt
 #SBATCH --output=/home1/05400/ibsantis/scripts/jobs/potentials/all_snapshots/RJ_subhalo_potential_all_snaps_%j.txt
 #SBATCH --mail-user=ibsantistevan@ucdavis.edu
