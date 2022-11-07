9fff96812061c957e1def934e4dd73c9010a9c84
Isaiah <ibsantistevan@ucdavis.edu>
2022-11-07 14:55:39 -0800

    Changing partition

--- summary_data.py
+++ summary_data.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 #SBATCH --job-name=summary_data_aligned_check
-#SBATCH --partition=high2m    # peloton high-mem node: 32 cores, 15.6 GB per core, 500 GB total
-##SBATCH --partition=high2    # peloton high-mem node: 32 cores, 15.6 GB per core, 500 GB total
+##SBATCH --partition=high2m    # peloton high-mem node: 32 cores, 15.6 GB per core, 500 GB total
+#SBATCH --partition=high2    # peloton high-mem node: 32 cores, 15.6 GB per core, 500 GB total
 ##SBATCH --mem=250G
 #SBATCH --mem=480G
 #SBATCH --nodes=1
