113be40aac01bacb13bb8bf5020a138eb6ad0750
Isaiah <ibsantistevan@ucdavis.edu>
2022-11-10 10:32:14 -0800

    Lots of changes, still need to run on LG hosts when data is done

--- energy_vs_time.py
+++ energy_vs_time.py
@@ -23,31 +23,32 @@ print('Read in the tools')
 
 
 ### Set path and initial parameters
-sim_data = orbit_io.OrbitRead(gal1='m12w', location='mac')
+sim_data = orbit_io.OrbitRead(gal1='Romeo', location='mac')
+sim_data.galaxy = sim_data.gal_1
 summary = summary_io.SummaryDataSort()
 summary_plot = summary_io.SummaryDataPlot()
 directory = sim_data.home_dir+'/orbit_data/plots/summary/paper_2_fix/energy'
 print('Set paths')
 
 # Set up snapshot array to loop through
-snaps = ut.simulation.read_snapshot_times(directory=sim_data.simulation_dir)
+snaps = ut.simulation.read_snapshot_times(directory=sim_data.home_dir+'/galaxies/m12i_res7100')
 tlb = snaps['time'][-1] - np.flip(snaps['time'])
 
 # Read in the potential data
-data = ut.io.file_hdf5(sim_data.home_dir+'/orbit_data/hdf5_files/potentials/all_snapshots/'+sim_data.galaxy+'_potentials_all', verbose=True)
+data = summary.data_read_potential_full(directory=sim_data.home_dir, hosts='lg_no_RR')
 # Read in the mass profile data
-mp = ut.io.file_hdf5(sim_data.home_dir+'/orbit_data/hdf5_files/mass_profiles/'+sim_data.galaxy+'_mass_profile_all', verbose=True)
+data_mp = summary.data_read_mass_profile(directory=sim_data.home_dir, hosts='lg_no_RR')
 # Read in all of the other data
-data_total = summary.data_read(directory=sim_data.home_dir, hosts='all_no_r', sim_type='baryon')
+data_total = summary.data_read(directory=sim_data.home_dir, hosts='lg_no_RR', sim_type='baryon')
 # Set up distance array (probably don't need it though...)
-rs = np.logspace(np.log10(5), np.log10(500), 25)
+rs = data_mp['rs']
 
 # Set the host potential at 100 kpc at z = 0
-phi_host_z0 = data['host.pot.100kpc'][-1] - data['host.pot.R200m'] - data['KE.at.Rvir']
+phi_host_z0 = data[sim_data.galaxy]['host.pot.100kpc'][-1] - data[sim_data.galaxy]['host.pot.R200m'] - data[sim_data.galaxy]['KE.at.Rvir']
 
 # Set up the enclosed mass ratio within 100-ish kpc
-M_enc_100kpc_z0 = mp['mass.profile'][-1][16]
-M_enc_100kpc_z = mp['mass.profile'][:,16]
+M_enc_100kpc_z0 = data_mp[sim_data.galaxy][-1][16]
+M_enc_100kpc_z = data_mp[sim_data.galaxy][:,16]
 #
 M_enc_ratio = M_enc_100kpc_z/M_enc_100kpc_z0
 
@@ -55,28 +56,28 @@ M_enc_ratio = M_enc_100kpc_z/M_enc_100kpc_z0
 phi_host_z = np.flip(phi_host_z0*M_enc_ratio) # Goes from z = 0 to some other z
 
 # Set up null array to save the normalized subhalo potentials to
-sub_pot = (-1)*np.ones(data['subhalo.pot'].shape)
-sub_energy = (-1)*np.ones(data['subhalo.pot'].shape)
-sub_pot_snaps = (-1)*np.ones(data['subhalo.pot'].shape, int)
-sub_pot_tlb = (-1)*np.ones(data['subhalo.pot'].shape)
+sub_pot = (-1)*np.ones(data[sim_data.galaxy]['subhalo.pot'].shape)
+sub_energy = (-1)*np.ones(data[sim_data.galaxy]['subhalo.pot'].shape)
+sub_pot_snaps = (-1)*np.ones(data[sim_data.galaxy]['subhalo.pot'].shape, int)
+sub_pot_tlb = (-1)*np.ones(data[sim_data.galaxy]['subhalo.pot'].shape)
 
 # Loop through all of the satellites
 for i in range(0, sub_pot.shape[0]):
     # Create a mask for the subhalo data
-    mask_sub = (np.flip(data['subhalo.pot'][i]) != -1)*np.isfinite(np.flip(data['subhalo.pot'][i]))*(data_total[sim_data.galaxy]['v.tot.sim'][i][:len(data['subhalo.pot'][i])] != -1)*np.isfinite(data_total[sim_data.galaxy]['v.tot.sim'][i][:len(data['subhalo.pot'][i])])*np.isfinite(np.flip(data['host.pot.100kpc']))
+    mask_sub = (np.flip(data[sim_data.galaxy]['subhalo.pot'][i]) != -1)*np.isfinite(np.flip(data[sim_data.galaxy]['subhalo.pot'][i]))*(data_total[sim_data.galaxy]['v.tot.sim'][i][:len(data[sim_data.galaxy]['subhalo.pot'][i])] != -1)*np.isfinite(data_total[sim_data.galaxy]['v.tot.sim'][i][:len(data[sim_data.galaxy]['subhalo.pot'][i])])*np.isfinite(np.flip(data[sim_data.galaxy]['host.pot.100kpc']))
     #print(np.sum(mask_sub))
     #
     # Create a mask for the host data
-    mask_host = (phi_host_z[:len(data['subhalo.pot'][i])] != 0)
+    mask_host = (phi_host_z[:len(data[sim_data.galaxy]['subhalo.pot'][i])] != 0)
     #
     # Calculate the normalized subhalo energy
-    sub_pot[i][mask_sub*mask_host] = np.flip(data['subhalo.pot'][i])[mask_sub*mask_host] - np.flip(data['host.pot.100kpc'])[mask_sub*mask_host] + phi_host_z[:len(data['subhalo.pot'][i])][mask_sub*mask_host]
+    sub_pot[i][mask_sub*mask_host] = np.flip(data[sim_data.galaxy]['subhalo.pot'][i])[mask_sub*mask_host] - np.flip(data[sim_data.galaxy]['host.pot.100kpc'])[mask_sub*mask_host] + phi_host_z[:len(data[sim_data.galaxy]['subhalo.pot'][i])][mask_sub*mask_host]
     # Keep which snapshots it has data for
-    sub_pot_snaps[i][mask_sub*mask_host] = np.flip(snaps['index'])[:len(data['subhalo.pot'][i])][mask_sub*mask_host]
-    sub_pot_tlb[i][mask_sub*mask_host] = tlb[:len(data['subhalo.pot'][i])][mask_sub*mask_host]
+    sub_pot_snaps[i][mask_sub*mask_host] = np.flip(snaps['index'])[:len(data[sim_data.galaxy]['subhalo.pot'][i])][mask_sub*mask_host]
+    sub_pot_tlb[i][mask_sub*mask_host] = tlb[:len(data[sim_data.galaxy]['subhalo.pot'][i])][mask_sub*mask_host]
     #
     # Calculate the total orbital energy
-    sub_energy[i][mask_sub*mask_host] = sub_pot[i][mask_sub*mask_host] + 0.5*(data_total[sim_data.galaxy]['v.tot.sim'][i][:len(data['subhalo.pot'][i])][mask_sub*mask_host])**2
+    sub_energy[i][mask_sub*mask_host] = sub_pot[i][mask_sub*mask_host] + 0.5*(data_total[sim_data.galaxy]['v.tot.sim'][i][:len(data[sim_data.galaxy]['subhalo.pot'][i])][mask_sub*mask_host])**2
 
 
 mask_low = (data_total[sim_data.galaxy]['infall.check'])*(data_total[sim_data.galaxy]['M.star.z0'] < 3e6)
@@ -87,10 +88,10 @@ plt.rcParams["font.family"] = "serif"
 f, axs = plt.subplots(2, 1, figsize=(10,12))
 #
 for i in range(0, np.sum(mask_low)):
-    m = (sub_energy[mask_low][i] != -1)
+    m = (sub_energy[mask_low][i] != -1)*(sub_pot_tlb[mask_low][i] < data_total[sim_data.galaxy]['first.infall.time.lb'][mask_low][i])
     axs[0].plot(sub_pot_tlb[mask_low][i][m], sub_energy[mask_low][i][m]/1e4, 'k', alpha=0.2)
 for i in range(0, np.sum(mask_high)):
-    m = (sub_energy[mask_high][i] != -1)
+    m = (sub_energy[mask_high][i] != -1)*(sub_pot_tlb[mask_high][i] < data_total[sim_data.galaxy]['first.infall.time.lb'][mask_high][i])
     axs[1].plot(sub_pot_tlb[mask_high][i][m], sub_energy[mask_high][i][m]/1e4, 'k', alpha=0.2)
 #
 cc = ut.cosmology.CosmologyClass()
@@ -118,9 +119,9 @@ axs[0].text(1.2,4.25,'$M_{\\rm star} < 10^{6.5} M_{\\odot}$',fontsize=20)
 axs[1].text(1.2,4.25,'$M_{\\rm star} > 10^{6.5} M_{\\odot}$',fontsize=20)
 #
 axs[0].set_xlim(0,13)
-axs[0].set_ylim(ymax=5.5)
+#axs[0].set_ylim(ymax=5.5)
 axs[1].set_xlim(0,13)
-axs[1].set_ylim(ymax=5.5)
+#axs[1].set_ylim(ymax=5.5)
 #
 axs[0].tick_params(axis='both', which='both', bottom=True, top=False, labelsize=28, labelbottom=False)
 axs[1].tick_params(axis='both', which='both', bottom=True, top=True, labelsize=28, labelbottom=True)
@@ -135,7 +136,7 @@ axs[1].set_xlabel('Lookback time [Gyr]', fontsize=32)
 plt.tight_layout()
 plt.subplots_adjust(wspace=0.12, hspace=0)
 #plt.show()
-plt.savefig(directory+'/'+sim_data.galaxy+'_E_vs_tlb_all.pdf')
+plt.savefig(directory+'/infall_satellites/'+sim_data.galaxy+'_E_vs_tlb_all_check.pdf')
 plt.close()
 
 
@@ -161,12 +162,14 @@ twosigm = 2.28
 thrsigp = 100
 thrsigm = 0
 #
+mask_infall = data_total[sim_data.galaxy]['infall.check']
+#
 for i in range(0, max_bin):
     mask_time = (tlb[:len(sub_energy[0])] > binss[i])*(tlb[:len(sub_energy[0])] < binss[i+1])
     temp = []
-    for j in range(0, len(sub_energy)):
-        mask_sub = (sub_energy[j] != -1)*np.isfinite(sub_energy[j])*mask_time
-        temp.append(sub_energy[j][mask_sub])
+    for j in range(0, np.sum(mask_infall)):
+        mask_sub = (sub_energy[mask_infall][j] != -1)*np.isfinite(sub_energy[mask_infall][j])*mask_time*(sub_pot_tlb[mask_infall][j] < data_total[sim_data.galaxy]['first.infall.time.lb'][mask_infall][j])
+        temp.append(sub_energy[mask_infall][j][mask_sub])
     meds[i] = np.nanmedian(np.hstack(temp))
     upper68[i] = np.nanpercentile(np.hstack(temp), onesigp)
     lower68[i] = np.nanpercentile(np.hstack(temp), onesigm)
@@ -201,7 +204,7 @@ axz.set_xlabel(axis_z_label, fontsize=30, labelpad=9)
 axz.tick_params(pad=3)
 #
 axs.set_xlim(0,max_bin)
-axs.set_ylim(-3,4)
+#axs.set_ylim(-3,4)
 #
 axs.tick_params(axis='both', which='both', bottom=True, top=False, labelsize=28, labelbottom=True)
 #
@@ -212,6 +215,6 @@ axs.set_xlabel('Lookback time [Gyr]', fontsize=32)
 #
 plt.tight_layout()
 #plt.show()
-#plt.savefig(directory+'/'+sim_data.galaxy+'_E_vs_tlb_median.pdf')
-plt.savefig(directory+'/'+sim_data.galaxy+'_E_vs_tlb_median_zoom.pdf')
+plt.savefig(directory+'/infall_satellites/'+sim_data.galaxy+'_E_vs_tlb_median.pdf')
+#plt.savefig(directory+'/'+sim_data.galaxy+'_E_vs_tlb_median_zoom.pdf')
 plt.close()
