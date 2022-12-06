ec577baa1a04c172f123a10619d1655748d82c13
Isaiah <ibsantistevan@ucdavis.edu>
2022-12-06 15:53:47 -0800

    Trying on another node

--- sat_potentials_model.py
+++ sat_potentials_model.py
@@ -2,12 +2,12 @@
 #SBATCH --job-name=model_potential
 ##SBATCH --partition=high2m    # peloton high-mem node: 32 cores, 15.6 GB per core, 500 GB total
 ##SBATCH --partition=high2    # peloton high-mem node: 32 cores, 15.6 GB per core, 500 GB total
-#SBATCH --partition=skx-normal
+#SBATCH --partition=icx-normal
 ##SBATCH --mem=250G
 ##SBATCH --mem=480G
 #SBATCH --nodes=1
 ##SBATCH --ntasks=4    # processes total
-#SBATCH --tasks-per-node=4
+#SBATCH --tasks-per-node=2
 #SBATCH --time=01:00:00
 ##SBATCH --output=/home/ibsantis/scripts/jobs/summary/model_potential%j.txt
 #SBATCH --output=/home1/05400/ibsantis/scripts/jobs/summary/model_potential%j.txt
