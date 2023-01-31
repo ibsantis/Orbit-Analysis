fb03543a1366179385124eb2d4cd135b38182707
Isaiah <ibsantistevan@ucdavis.edu>
2023-01-31 09:11:05 -0800

    Running again on m12b on Peloton

--- sat_potentials_all_snap.py
+++ sat_potentials_all_snap.py
@@ -1,20 +1,20 @@
 #!/usr/bin/env python3
-#SBATCH --job-name=TL_subhalo_potential_all_snaps
-##SBATCH --partition=high2m    # peloton high-mem node: 32 cores, 15.6 GB per core, 500 GB total
-#SBATCH --partition=skx-normal
-##SBATCH --mem=500G
+#SBATCH --job-name=m12b_subhalo_potential_all_snaps
+#SBATCH --partition=high2m    # peloton high-mem node: 32 cores, 15.6 GB per core, 500 GB total
+##SBATCH --partition=skx-normal
+#SBATCH --mem=500G
 #SBATCH --nodes=1
-##SBATCH --ntasks=3    # processes total
-#SBATCH --tasks-per-node=1    # MPI tasks per node
-##SBATCH --cpus-per-task=1    # OpenMP threads per MPI task
-#SBATCH --time=02:30:00
-##SBATCH --output=/home/ibsantis/scripts/jobs/potentials/all_snapshots/m12w_subhalo_potential_all_snaps_%j.txt
-#SBATCH --output=/home1/05400/ibsantis/scripts/jobs/potentials/all_snapshots/TL_subhalo_potential_all_snaps_%j.txt
+#SBATCH --ntasks=3    # processes total
+##SBATCH --tasks-per-node=1    # MPI tasks per node
+#SBATCH --cpus-per-task=1    # OpenMP threads per MPI task
+#SBATCH --time=04:00:00
+#SBATCH --output=/home/ibsantis/scripts/jobs/potentials/all_snapshots/m12b_subhalo_potential_all_snaps_%j.txt
+##SBATCH --output=/home1/05400/ibsantis/scripts/jobs/potentials/all_snapshots/TL_subhalo_potential_all_snaps_%j.txt
 #SBATCH --mail-user=ibsantistevan@ucdavis.edu
 #SBATCH --mail-type=fail
 #SBATCH --mail-type=end
 #SBATCH --mail-type=begin
-#SBATCH --account=TG-AST140064
+##SBATCH --account=TG-AST140064
 
 """
 
@@ -48,8 +48,8 @@ import sys
 print('Read in the tools')
 
 ### Set path and initial parameters
-loc = 'stampede'
-sim_data = orbit_io.OrbitRead(gal1='Thelma', location=loc)
+loc = 'peloton'
+sim_data = orbit_io.OrbitRead(gal1='m12b', location=loc)
 print('Set paths')
 
 # Read in snapshot dictionary and the halo tree
@@ -60,8 +60,8 @@ orbits = orbit_io.OrbitAnalysis(tree=halt, gal1=sim_data.gal_1, location=loc, ho
 print('Read in halo tree and set up subhalo indices')
 
 # Set up the snapshot array to loop through
-#snaps = np.flip(snaps['index'])[:len(orbits.sub_inds[0])]
-snaps = np.arange(int(sys.argv[1]), int(sys.argv[2]), -1)
+snaps = np.flip(snaps['index'])[:len(orbits.sub_inds[0])]
+#snaps = np.arange(int(sys.argv[1]), int(sys.argv[2]), -1)
 
 def calc_sub_potential(snap, simdata, orbit_class):
     #
