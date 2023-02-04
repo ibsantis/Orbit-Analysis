dc95be77ab6433a6d56ada58154573066e690f40
Isaiah <ibsantistevan@ucdavis.edu>
2023-02-04 12:12:54 -0800

    Running on m12i on Peloton

--- sat_potentials_all_snap.py
+++ sat_potentials_all_snap.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-#SBATCH --job-name=m12f_subhalo_potential_all_snaps
+#SBATCH --job-name=m12i_subhalo_potential_all_snaps
 ##SBATCH --job-name=RJ_subhalo_potential_all_snaps
 ##SBATCH --partition=high2m    # peloton high-mem node: 32 cores, 15.6 GB per core, 500 GB total
 #SBATCH --partition=high2
@@ -11,7 +11,7 @@
 ##SBATCH --tasks-per-node=1    # MPI tasks per node
 #SBATCH --cpus-per-task=1    # OpenMP threads per MPI task
 #SBATCH --time=06:00:00
-#SBATCH --output=/home/ibsantis/scripts/jobs/potentials/all_snapshots/m12f_subhalo_potential_all_snaps_%j.txt
+#SBATCH --output=/home/ibsantis/scripts/jobs/potentials/all_snapshots/m12i_subhalo_potential_all_snaps_%j.txt
 ##SBATCH --output=/home1/05400/ibsantis/scripts/jobs/potentials/all_snapshots/RJ_subhalo_potential_all_snaps_%j.txt
 #SBATCH --mail-user=ibsantistevan@ucdavis.edu
 #SBATCH --mail-type=fail
@@ -52,7 +52,7 @@ print('Read in the tools')
 
 ### Set path and initial parameters
 loc = 'peloton'
-sim_data = orbit_io.OrbitRead(gal1='m12f', location=loc)
+sim_data = orbit_io.OrbitRead(gal1='m12i', location=loc)
 print('Set paths')
 
 # Read in snapshot dictionary and the halo tree
