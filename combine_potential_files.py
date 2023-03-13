96edb310dfe15498b563ec9c57cd6f4a5a5a54fa
Isaiah <ibsantistevan@ucdavis.edu>
2023-03-13 09:26:12 -0700

    Switched 100 to 500 kpc.

--- combine_potential_files.py
+++ combine_potential_files.py
@@ -19,13 +19,14 @@ print('Read in the tools')
 
 
 ### Set path and initial parameters
-sim_data = orbit_io.OrbitRead(gal1='m12i', location='mac')
-sim_data.galaxy = 'm12i'
+sim_data = orbit_io.OrbitRead(gal1='Romeo', location='mac')
+sim_data.galaxy = 'Juliet'
 print('Set paths')
 
 # Set up snapshot array to loop through
-snaps = ut.simulation.read_snapshot_times(directory=sim_data.simulation_dir)
-snaps = snaps['index'][10:]
+#snaps = ut.simulation.read_snapshot_times(directory=sim_data.simulation_dir)
+snaps = ut.simulation.read_snapshot_times(directory=sim_data.home_dir+'/galaxies/R_J')
+snaps = snaps['index'][3:]
 
 # Read in the z = 0 data so that I can get the length of the arrays
 data_z0 = ut.io.file_hdf5(sim_data.home_dir+'/orbit_data/hdf5_files/potentials_new/all_snapshots/'+sim_data.galaxy+'/'+sim_data.galaxy+'_potentials_600', verbose=True)
@@ -34,13 +35,13 @@ data_dict = dict()
 data_dict['snapshot'] = snaps
 data_dict['KE.at.Rvir'] = data_z0['KE.at.Rvir']
 data_dict['host.pot.R200m'] = data_z0['host.potential.R200m']
-data_dict['host.pot.100kpc'] = np.zeros(len(snaps))
+data_dict['host.pot.500kpc'] = np.zeros(len(snaps))
 data_dict['subhalo.inds'] = np.zeros((len(data_z0['halo.inds']), len(snaps)))
 data_dict['subhalo.pot'] = np.zeros((len(data_z0['halo.inds']), len(snaps)))
 #
 for i in range(0, len(snaps)):
     data = ut.io.file_hdf5(sim_data.home_dir+'/orbit_data/hdf5_files/potentials_new/all_snapshots/'+sim_data.galaxy+'/'+sim_data.galaxy+'_potentials_'+str(snaps[i]), verbose=True)
-    data_dict['host.pot.100kpc'][i] = data['host.potential.100kpc']
+    data_dict['host.pot.500kpc'][i] = data['host.potential.500kpc']
     data_dict['subhalo.inds'][:,i] = data['halo.inds']
     data_dict['subhalo.pot'][:,i] = data['subhalo.potential']
 #
