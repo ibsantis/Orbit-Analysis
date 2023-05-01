8e594e81bd97e3282522f43019ed5ca62ac1921c
Isaiah <ibsantistevan@ucdavis.edu>
2023-05-01 09:41:39 -0700

    Made new plots with the new data

--- point_mass_plots.py
+++ point_mass_plots.py
@@ -44,6 +44,7 @@ print('Set paths')
 # Initialize the classes, read in the data, and create data masks
 summary = summary_io.SummaryDataSort()
 summary_plot = summary_io.SummaryDataPlot()
+#
 data_total = summary.data_read(directory=sim_data.home_dir, hosts='all_no_r', sim_type='baryon')
 masks_infall = summary.data_mask(data_total, peri_sim=False, peri_model=False, hosts='all_no_r')
 masks_infall_peri = summary.data_mask(data_total, peri_sim=True, peri_model=False, hosts='all_no_r')
@@ -71,8 +72,8 @@ t_in_mod_pm = summary.first_infall(data_total_point, masks_infall_point, selecti
 mask_finite = np.isfinite(t_in_mod)*(t_in_mod != -1)
 mask_finite_pm = np.isfinite(t_in_mod_pm)*(t_in_mod_pm != -1)
 #
-summary_plot.plot_hist(x=t_in_mod_pm[mask_finite*mask_finite_pm] - t_in_mod[mask_finite*mask_finite_pm], xtype='t.infall.text', binsize=0.1, pdf=True, xlimits=(-4,2), x_labels='$t_{\\rm in,lb,point}-t_{\\rm in,lb,disk}$ [Gyr]', file_path_and_name=directory+'/infall_hist.pdf')
-summary_plot.median_plot(x=t_in_mod[mask_finite*mask_finite_pm], y=(t_in_mod_pm[mask_finite*mask_finite_pm] - t_in_mod[mask_finite*mask_finite_pm]), xtype='t.infall.text', ytype='delta_t', binsize=0.5, limits=((13.7,0),(-4, 1.5)), hl=True, axis_labels=['$t_{\\rm infall,lb,disk}$ [Gyr]', '$t_{\\rm infall,lb,point}-t_{\\rm infall,lb,disk}$ [Gyr]'], file_path_and_name=directory+'/delta_infall_vs_infall.pdf')
+summary_plot.plot_hist(x=t_in_mod_pm[mask_finite*mask_finite_pm] - t_in_mod[mask_finite*mask_finite_pm], xtype='t.infall.text', binsize=0.1, pdf=True, xlimits=(-1,1), x_labels='$t_{\\rm in,lb,point}-t_{\\rm in,lb,disk}$ [Gyr]', file_path_and_name=directory+'/infall_hist.pdf')
+summary_plot.median_plot(x=t_in_mod[mask_finite*mask_finite_pm], y=(t_in_mod_pm[mask_finite*mask_finite_pm] - t_in_mod[mask_finite*mask_finite_pm]), xtype='t.infall.text', ytype='delta_t', binsize=0.5, limits=((13.7,0),(-1, 0.5)), hl=True, axis_labels=['$t_{\\rm infall,lb,disk}$ [Gyr]', '$t_{\\rm infall,lb,point}-t_{\\rm infall,lb,disk}$ [Gyr]'], file_path_and_name=directory+'/delta_infall_vs_infall.pdf')
 #
 t_in_mod_fixed = summary.infall_fixed(data_total, masks_infall, oversample=True, hosts='all_no_r', sim_type='baryon')
 t_in_mod_fixed_pm = summary.infall_fixed(data_total_point, masks_infall_point, oversample=True, hosts='all_no_r', sim_type='baryon')
@@ -80,7 +81,7 @@ mask_finite_fixed = np.isfinite(t_in_mod_fixed)*(t_in_mod_fixed != -1)
 mask_finite_fixed_pm = np.isfinite(t_in_mod_fixed_pm)*(t_in_mod_fixed_pm != -1)
 #
 summary_plot.plot_hist(x=(t_in_mod_fixed_pm[mask_finite_fixed*mask_finite_fixed_pm] - t_in_mod_fixed[mask_finite_fixed*mask_finite_fixed_pm]), xtype='t.infall.text', binsize=0.05, xlimits=(-0.5,0.5), pdf=True, x_labels='$t_{\\rm in,lb,point,fixed}-t_{\\rm in,lb,disk,fixed}$ [Gyr]', file_path_and_name=directory+'/infall_fixed_hist.pdf')
-summary_plot.median_plot(x=t_in_mod_fixed[mask_finite_fixed*mask_finite_fixed_pm], y=(t_in_mod_fixed_pm[mask_finite_fixed*mask_finite_fixed_pm] - t_in_mod_fixed[mask_finite_fixed*mask_finite_fixed_pm]), xtype='t.infall.text', ytype='delta_t', binsize=0.5, limits=((12,0),(-1, 1)), hl=True, axis_labels=['$t_{\\rm infall,lb,disk}$ [Gyr]', '$t_{\\rm infall,lb,point}-t_{\\rm infall,lb,disk}$ [Gyr]'], file_path_and_name=directory+'/delta_infall_vs_infall_fixed.pdf')
+summary_plot.median_plot(x=t_in_mod_fixed[mask_finite_fixed*mask_finite_fixed_pm], y=(t_in_mod_fixed_pm[mask_finite_fixed*mask_finite_fixed_pm] - t_in_mod_fixed[mask_finite_fixed*mask_finite_fixed_pm]), xtype='t.infall.text', ytype='delta_t', binsize=0.5, limits=((12,0),(-0.5, 0.5)), hl=True, axis_labels=['$t_{\\rm infall,lb,disk}$ [Gyr]', '$t_{\\rm infall,lb,point}-t_{\\rm infall,lb,disk}$ [Gyr]'], file_path_and_name=directory+'/delta_infall_vs_infall_fixed.pdf')
 
 
 """
@@ -89,17 +90,17 @@ summary_plot.median_plot(x=t_in_mod_fixed[mask_finite_fixed*mask_finite_fixed_pm
 d_rec_mod = summary.dperi_recent(data_total, masks_infall_peri, selection='model', oversample=True, hosts='all_no_r', sim_type='baryon')
 d_rec_mod_pm = summary.dperi_recent(data_total_point, masks_infall_peri_point, selection='model', oversample=True, hosts='all_no_r', sim_type='baryon')
 #
-summary_plot.plot_hist(x=(d_rec_mod_pm-d_rec_mod)/d_rec_mod, xtype='d.peri.text', binsize=0.01, xlimits=(-0.3,0.5), pdf=True, x_labels='$(d_{\\rm peri,rec,point}-d_{\\rm peri,rec,disk})/d_{\\rm peri,rec,disk}$', file_path_and_name=directory+'/d_peri_rec_hist.pdf')
-summary_plot.median_plot(x=d_rec_mod, y=(d_rec_mod_pm-d_rec_mod), xtype='d.peri.text', ytype='d.peri.text', binsize=25, limits=((0,300),(-5,25)), hl=True, axis_labels=['$d_{\\rm peri,rec,disk}$ [kpc]', '$d_{\\rm peri,rec,point}-d_{\\rm peri,rec,disk}$ [kpc]'], file_path_and_name=directory+'/delta_d_peri_rec_vs_rec.pdf')
-summary_plot.median_plot(x=d_rec_mod, y=(d_rec_mod_pm-d_rec_mod)/d_rec_mod, xtype='d.peri.text', ytype='d.peri.text', binsize=25, limits=((0,300),(-0.01,0.2)), hl=True, axis_labels=['$d_{\\rm peri,rec,disk}$ [kpc]', '$(d_{\\rm peri,rec,point}-d_{\\rm peri,rec,disk})/d_{\\rm peri,rec,disk}$'], file_path_and_name=directory+'/d_peri_rec_frac_vs_rec.pdf')
+summary_plot.plot_hist(x=(d_rec_mod_pm-d_rec_mod)/d_rec_mod, xtype='d.peri.text', binsize=0.01, xlimits=(-0.1,0.1), pdf=True, x_labels='$(d_{\\rm peri,rec,point}-d_{\\rm peri,rec,disk})/d_{\\rm peri,rec,disk}$', file_path_and_name=directory+'/d_peri_rec_hist.pdf')
+summary_plot.median_plot(x=d_rec_mod, y=(d_rec_mod_pm-d_rec_mod), xtype='d.peri.text', ytype='d.peri.text', binsize=25, limits=((0,300),(-2,5)), hl=True, axis_labels=['$d_{\\rm peri,rec,disk}$ [kpc]', '$d_{\\rm peri,rec,point}-d_{\\rm peri,rec,disk}$ [kpc]'], file_path_and_name=directory+'/delta_d_peri_rec_vs_rec.pdf')
+summary_plot.median_plot(x=d_rec_mod, y=(d_rec_mod_pm-d_rec_mod)/d_rec_mod, xtype='d.peri.text', ytype='d.peri.text', binsize=25, limits=((0,300),(-0.01,0.03)), hl=True, axis_labels=['$d_{\\rm peri,rec,disk}$ [kpc]', '$(d_{\\rm peri,rec,point}-d_{\\rm peri,rec,disk})/d_{\\rm peri,rec,disk}$'], file_path_and_name=directory+'/d_peri_rec_frac_vs_rec.pdf')
 
 
 d_min_mod = summary.dperi_min(data_total, masks_infall_peri, selection='model', oversample=True, hosts='all_no_r', sim_type='baryon')
 d_min_mod_pm = summary.dperi_min(data_total_point, masks_infall_peri_point, selection='model', oversample=True, hosts='all_no_r', sim_type='baryon')
 #
-summary_plot.plot_hist(x=(d_min_mod_pm-d_min_mod)/d_min_mod, xtype='d.peri.text', binsize=0.01, xlimits=(-0.3,0.5), pdf=True, x_labels='$(d_{\\rm peri,min,point}-d_{\\rm peri,min,disk})/d_{\\rm peri,min,disk}$', file_path_and_name=directory+'/d_peri_min_hist.pdf')
-summary_plot.median_plot(x=d_min_mod, y=(d_min_mod_pm-d_min_mod), xtype='d.peri.text', ytype='d.peri.text', binsize=25, limits=((0,300),(-5,25)), hl=True, axis_labels=['$d_{\\rm peri,min,disk}$ [kpc]', '$d_{\\rm peri,min,point}-d_{\\rm peri,min,disk}$ [kpc]'], file_path_and_name=directory+'/delta_d_peri_min_vs_min.pdf')
-summary_plot.median_plot(x=d_min_mod, y=(d_min_mod_pm-d_min_mod)/d_min_mod, xtype='d.peri.text', ytype='d.peri.text', binsize=25, limits=((0,300),(-0.01,0.2)), hl=True, axis_labels=['$d_{\\rm peri,min,disk}$ [kpc]', '$(d_{\\rm peri,min,point}-d_{\\rm peri,min,disk})/d_{\\rm peri,min,disk}$'], file_path_and_name=directory+'/d_peri_min_frac_vs_min.pdf')
+summary_plot.plot_hist(x=(d_min_mod_pm-d_min_mod)/d_min_mod, xtype='d.peri.text', binsize=0.01, xlimits=(-0.1,0.1), pdf=True, x_labels='$(d_{\\rm peri,min,point}-d_{\\rm peri,min,disk})/d_{\\rm peri,min,disk}$', file_path_and_name=directory+'/d_peri_min_hist.pdf')
+summary_plot.median_plot(x=d_min_mod, y=(d_min_mod_pm-d_min_mod), xtype='d.peri.text', ytype='d.peri.text', binsize=25, limits=((0,300),(-2,5)), hl=True, axis_labels=['$d_{\\rm peri,min,disk}$ [kpc]', '$d_{\\rm peri,min,point}-d_{\\rm peri,min,disk}$ [kpc]'], file_path_and_name=directory+'/delta_d_peri_min_vs_min.pdf')
+summary_plot.median_plot(x=d_min_mod, y=(d_min_mod_pm-d_min_mod)/d_min_mod, xtype='d.peri.text', ytype='d.peri.text', binsize=25, limits=((0,300),(-0.01,0.03)), hl=True, axis_labels=['$d_{\\rm peri,min,disk}$ [kpc]', '$(d_{\\rm peri,min,point}-d_{\\rm peri,min,disk})/d_{\\rm peri,min,disk}$'], file_path_and_name=directory+'/d_peri_min_frac_vs_min.pdf')
 
 
 """
@@ -109,8 +110,8 @@ t_rec_mod = summary.tperi_recent(data_total, masks_infall_peri, selection='model
 t_rec_mod_pm = summary.tperi_recent(data_total_point, masks_infall_peri_point, selection='model', oversample=True, hosts='all_no_r', sim_type='baryon')
 mask = np.isfinite(t_rec_mod)*np.isfinite(t_rec_mod_pm)*(t_rec_mod != -1)*(t_rec_mod_pm != -1)
 #
-summary_plot.plot_hist(x=(t_rec_mod_pm[mask]-t_rec_mod[mask]), xtype='t.peri.text', binsize=0.1, xlimits=(-1,2), pdf=True, x_labels='$(t_{\\rm peri,rec,point}-t_{\\rm peri,rec,disk})$ [Gyr]', file_path_and_name=directory+'/t_peri_rec_hist.pdf')
-summary_plot.median_plot(x=t_rec_mod, y=(t_rec_mod_pm-t_rec_mod), xtype='t.peri.text', ytype='delta_t', binsize=0.5, limits=((9,0),(-0.5,1.5)), hl=True, axis_labels=['$t_{\\rm peri,rec,disk}$ [kpc]', '$t_{\\rm peri,rec,point}-t_{\\rm peri,rec,disk}$ [Gyr]'], file_path_and_name=directory+'/delta_t_peri_rec_vs_rec.pdf')
+summary_plot.plot_hist(x=(t_rec_mod_pm[mask]-t_rec_mod[mask]), xtype='t.peri.text', binsize=0.05, xlimits=(-0.2,0.2), pdf=True, x_labels='$(t_{\\rm peri,rec,point}-t_{\\rm peri,rec,disk})$ [Gyr]', file_path_and_name=directory+'/t_peri_rec_hist.pdf')
+summary_plot.median_plot(x=t_rec_mod, y=(t_rec_mod_pm-t_rec_mod), xtype='t.peri.text', ytype='delta_t', binsize=0.5, limits=((9,0),(-0.1,0.2)), hl=True, axis_labels=['$t_{\\rm peri,rec,disk}$ [kpc]', '$t_{\\rm peri,rec,point}-t_{\\rm peri,rec,disk}$ [Gyr]'], file_path_and_name=directory+'/delta_t_peri_rec_vs_rec.pdf')
 
 
 """
@@ -119,8 +120,8 @@ summary_plot.median_plot(x=t_rec_mod, y=(t_rec_mod_pm-t_rec_mod), xtype='t.peri.
 v_rec_mod = summary.vperi_recent(data_total, masks_infall_peri, selection='model', oversample=True, hosts='all_no_r', sim_type='baryon')
 v_rec_mod_pm = summary.vperi_recent(data_total_point, masks_infall_peri_point, selection='model', oversample=True, hosts='all_no_r', sim_type='baryon')
 #
-summary_plot.plot_hist(x=(v_rec_mod_pm-v_rec_mod), xtype='v.peri.text', binsize=5, xlimits=(-100,10), pdf=True, x_labels='$(v_{\\rm peri,rec,point}-v_{\\rm peri,rec,disk})$ [km s$^{-1}$]', file_path_and_name=directory+'/v_peri_rec_hist.pdf')
-summary_plot.median_plot(x=v_rec_mod, y=(v_rec_mod_pm-v_rec_mod), xtype='v.peri.text', ytype='v.peri.text', binsize=25, limits=((0,650),(-150,10)), hl=True, axis_labels=['$v_{\\rm peri,rec,disk}$ [km s$^{-1}$]', '$v_{\\rm peri,rec,point}-v_{\\rm peri,rec,disk}$ [km s$^{-1}$]'], file_path_and_name=directory+'/delta_v_peri_rec_vs_rec.pdf')
+summary_plot.plot_hist(x=(v_rec_mod_pm-v_rec_mod), xtype='v.peri.text', binsize=1, xlimits=(-20,20), pdf=True, x_labels='$(v_{\\rm peri,rec,point}-v_{\\rm peri,rec,disk})$ [km s$^{-1}$]', file_path_and_name=directory+'/v_peri_rec_hist.pdf')
+summary_plot.median_plot(x=v_rec_mod, y=(v_rec_mod_pm-v_rec_mod), xtype='v.peri.text', ytype='v.peri.text', binsize=25, limits=((0,650),(-10,20)), hl=True, axis_labels=['$v_{\\rm peri,rec,disk}$ [km s$^{-1}$]', '$v_{\\rm peri,rec,point}-v_{\\rm peri,rec,disk}$ [km s$^{-1}$]'], file_path_and_name=directory+'/delta_v_peri_rec_vs_rec.pdf')
 
 
 """
@@ -129,14 +130,14 @@ summary_plot.median_plot(x=v_rec_mod, y=(v_rec_mod_pm-v_rec_mod), xtype='v.peri.
 n_mod = summary.nperi_model(data_total, masks_infall, selection='model', oversample=True, hosts='all_no_r', sim_type='baryon')
 n_mod_pm = summary.nperi_model(data_total_point, masks_infall_point, selection='model', oversample=True, hosts='all_no_r', sim_type='baryon')
 #
-summary_plot.plot_hist(x=(n_mod_pm-n_mod), xtype='N.peri.text', binsize=1, xlimits=(-3,1), pdf=True, x_labels='$(N_{\\rm peri,point}-N_{\\rm peri,disk})$', file_path_and_name=directory+'/N_peri_hist.pdf')
-summary_plot.median_plot(x=n_mod, y=(n_mod_pm-n_mod), xtype='N.peri.text', ytype='N.peri.text', binsize=1, limits=((0,13),(-3,0.1)), hl=True, axis_labels=['$N_{\\rm peri,disk}$', '$N_{\\rm peri,point}-N_{\\rm peri,disk}$'], file_path_and_name=directory+'/delta_N_peri.pdf')
+summary_plot.plot_hist(x=(n_mod_pm-n_mod), xtype='N.peri.text', binsize=1, xlimits=(-3,3), pdf=True, x_labels='$(N_{\\rm peri,point}-N_{\\rm peri,disk})$', file_path_and_name=directory+'/N_peri_hist.pdf')
+summary_plot.median_plot(x=n_mod, y=(n_mod_pm-n_mod), xtype='N.peri.text', ytype='N.peri.text', binsize=1, limits=((0,13),(-1,0.1)), hl=True, axis_labels=['$N_{\\rm peri,disk}$', '$N_{\\rm peri,point}-N_{\\rm peri,disk}$'], file_path_and_name=directory+'/delta_N_peri.pdf')
 
 n_mod_fixed = summary.nperi_model(data_total, masks_infall, selection='model.R200m', oversample=True, hosts='all_no_r', sim_type='baryon')
 n_mod_fixed_pm = summary.nperi_model(data_total_point, masks_infall_point, selection='model.R200m', oversample=True, hosts='all_no_r', sim_type='baryon')
 #
-summary_plot.plot_hist(x=(n_mod_fixed_pm-n_mod_fixed), xtype='N.peri.text', binsize=1, xlimits=(-3,1), pdf=True, x_labels='$(N_{\\rm peri,point}-N_{\\rm peri,disk})$', file_path_and_name=directory+'/N_peri_fixed_hist.pdf')
-summary_plot.median_plot(x=n_mod_fixed, y=(n_mod_fixed_pm-n_mod_fixed), xtype='N.peri.text', ytype='N.peri.text', binsize=1, limits=((0,13),(-3,0.1)), hl=True, axis_labels=['$N_{\\rm peri,disk}$', '$N_{\\rm peri,point}-N_{\\rm peri,disk}$'], file_path_and_name=directory+'/delta_N_peri_fixed.pdf')
+summary_plot.plot_hist(x=(n_mod_fixed_pm-n_mod_fixed), xtype='N.peri.text', binsize=1, xlimits=(-3,3), pdf=True, x_labels='$(N_{\\rm peri,point}-N_{\\rm peri,disk})$', file_path_and_name=directory+'/N_peri_fixed_hist.pdf')
+summary_plot.median_plot(x=n_mod_fixed, y=(n_mod_fixed_pm-n_mod_fixed), xtype='N.peri.text', ytype='N.peri.text', binsize=1, limits=((0,13),(-1,0.1)), hl=True, axis_labels=['$N_{\\rm peri,disk}$', '$N_{\\rm peri,point}-N_{\\rm peri,disk}$'], file_path_and_name=directory+'/delta_N_peri_fixed.pdf')
 
 
 """
@@ -145,5 +146,5 @@ summary_plot.median_plot(x=n_mod_fixed, y=(n_mod_fixed_pm-n_mod_fixed), xtype='N
 dapo_rec_mod = summary.dapo_recent(data_total, masks_infall_apo, selection='model', oversample=True, hosts='all_no_r', sim_type='baryon')
 dapo_rec_mod_pm = summary.dapo_recent(data_total_point, masks_infall_apo_point, selection='model', oversample=True, hosts='all_no_r', sim_type='baryon')
 #
-summary_plot.plot_hist(x=(dapo_rec_mod_pm-dapo_rec_mod)/dapo_rec_mod, xtype='d.apo.text', binsize=0.01, xlimits=(-0.1,0.5), pdf=True, x_labels='$(d_{\\rm apo,rec,point}-d_{\\rm apo,rec,disk})/d_{\\rm apo,rec,disk}$', file_path_and_name=directory+'/d_apo_rec_hist.pdf')
-summary_plot.median_plot(x=dapo_rec_mod, y=(dapo_rec_mod_pm-dapo_rec_mod)/dapo_rec_mod, xtype='d.apo.text', ytype='d.apo.text', binsize=25, limits=((0,500),(-0.05,0.3)), hl=True, axis_labels=['$d_{\\rm apo,rec,disk}$ [kpc]', '$(d_{\\rm apo,rec,point}-d_{\\rm apo,rec,disk})/d_{\\rm apo,rec,disk}$'], file_path_and_name=directory+'/d_apo_rec_frac_vs_rec.pdf')
+summary_plot.plot_hist(x=(dapo_rec_mod_pm-dapo_rec_mod)/dapo_rec_mod, xtype='d.apo.text', binsize=0.01, xlimits=(-0.1,0.1), pdf=True, x_labels='$(d_{\\rm apo,rec,point}-d_{\\rm apo,rec,disk})/d_{\\rm apo,rec,disk}$', file_path_and_name=directory+'/d_apo_rec_hist.pdf')
+summary_plot.median_plot(x=dapo_rec_mod, y=(dapo_rec_mod_pm-dapo_rec_mod)/dapo_rec_mod, xtype='d.apo.text', ytype='d.apo.text', binsize=25, limits=((0,500),(-0.01,0.02)), hl=True, axis_labels=['$d_{\\rm apo,rec,disk}$ [kpc]', '$(d_{\\rm apo,rec,point}-d_{\\rm apo,rec,disk})/d_{\\rm apo,rec,disk}$'], file_path_and_name=directory+'/d_apo_rec_frac_vs_rec.pdf')
