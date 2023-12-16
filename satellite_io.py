be67ed326fa10b48315636a0e37d50963a819c6c
Isaiah <ibsantistevan@ucdavis.edu>
2023-12-15 16:07:56 -0800

    added binning scheme to pipeline

--- satellite_io.py
+++ satellite_io.py
@@ -565,3 +565,40 @@ class SatelliteAnalysis(SatelliteRead):
             #
         #
         return d
+
+    def binning_scheme(self, x, xtype, binsize, binedges=None):
+        """
+        DESCRIPTION:
+            Function that makes bins... finish later.
+        """
+        if 'M.' in xtype:
+            x = np.log10(x)
+        #
+        if binedges:
+            bin_num = int((binedges[1]-binedges[0])/binsize + 1)
+            bins = np.linspace(binedges[0], binedges[1], bin_num)
+            half_bin = (bins[1]-bins[0])/2
+        #
+        else:
+            if 'N.' not in xtype:
+                minn = binsize*np.floor(np.nanmin(x)/binsize)
+                maxx = binsize*np.ceil(np.nanmax(x)/binsize)
+                if minn < 0:
+                    bin_num = int(np.around((np.abs(minn)+np.abs(maxx))/binsize+1))
+                else:
+                    bin_num = int(np.around((np.abs(maxx)-np.abs(minn))/binsize+1))
+                bins = np.linspace(minn, maxx, bin_num)
+                half_bin = (bins[1]-bins[0])/2
+            #
+            elif 'N.' in xtype:
+                minn = int(binsize*np.floor(np.nanmin(x)/binsize))-0.5
+                maxx = int(binsize*np.ceil(np.nanmax(x)/binsize))+0.5
+                if minn < 0:
+                    bin_num = int((np.abs(maxx)+np.abs(minn))/binsize+1)
+                else:
+                    bin_num = int((np.abs(maxx)-np.abs(minn))/binsize+1)
+                bins = np.linspace(minn, maxx, bin_num)
+                #
+                half_bin = (bins[1]-bins[0])/2
+        #
+        return bins, half_bin
