205ab8ab79c8cb89af5c311cf5e9f3c591a3938d
Isaiah <ibsantistevan@ucdavis.edu>
2023-12-22 11:22:11 -0800

    fixed binning again

--- satellite_io.py
+++ satellite_io.py
@@ -595,7 +595,7 @@ class SatelliteAnalysis(SatelliteRead):
                 if (minn < 0) and (maxx > 0):
                     bin_num = int(np.around((np.abs(minn)+np.abs(maxx))/binsize+1))
                 elif (minn < 0) and (maxx < 0):
-                    bin_num = np.abs(int(np.around((np.abs(maxx)-np.abs(minn))/binsize+1)))
+                    bin_num = np.abs(int(np.around((np.abs(maxx)-np.abs(minn))/binsize)))+1
                 else:
                     bin_num = int(np.around((np.abs(maxx)-np.abs(minn))/binsize+1))
                 bins = np.linspace(minn, maxx, bin_num)
