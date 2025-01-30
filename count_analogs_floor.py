416b8562b4eb18695167b1a0a99e192076fc8f10
Isaiah <ibsantistevan@ucdavis.edu>
2025-01-29 17:16:10 -0800

    Count the num of analogs for a given floor select

---
+++ count_analogs_floor.py
@@ -0,0 +1,61 @@
+#!/usr/bin/python3
+
+"""
+    =============================
+    = Paper III Summary Table =
+    =============================
+
+    ...
+"""
+
+# Import packages
+import orbit_io
+import halo_analysis as halo
+import gizmo_analysis as gizmo
+import utilities as ut
+import numpy as np
+import pandas as pd
+import satellite_io
+import matplotlib
+from matplotlib import pyplot as plt
+import time
+print('Read in the tools')
+
+### Set path and initial parameters
+loc = 'mac'
+sim_data = satellite_io.SatelliteRead(gal1='m12i', location=loc)
+sat_analysis = satellite_io.SatelliteAnalysis(gal1='m12i', location=loc)
+#
+print('Set paths')
+
+# Read in the snapshot dictionary and the entire tree
+mw_sats_1Mpc =     ['Antlia II', 'Aquarius II', 'Aquarius III', 'Bootes I', 'Bootes II', 'Bootes III', \
+                    'Bootes IV', 'Bootes V', 'Canes Venatici I', 'Canes Venatici II', 'Carina', 'Carina II', \
+                    'Carina III', 'Centaurus I', 'Cetus II', 'Cetus III', 'Columba I', 'Coma Berenices', \
+                    'Crater II', 'Draco', 'Draco II', 'Eridanus II', 'Eridanus III', 'Eridanus IV', \
+                    'Fornax', 'Grus I', 'Grus II', 'Hercules', 'Horologium I', 'Horologium II', \
+                    'Hydra II', 'Hydrus I', 'Indus I', 'Leo I', 'Leo II', 'Leo IV', \
+                    'Leo V', 'Leo VI', 'Leo A', 'Leo T', 'Leo Minor I', 'Pegasus III', \
+                    'Pegasus IV', 'Phoenix I', 'Phoenix II', 'Pictor I', 'Pictor II', 'Pisces II', \
+                    'Reticulum II', 'Reticulum III', 'Sagittarius', 'Sagittarius II', 'Sculptor', 'Segue 1', \
+                    'Segue 2', 'Sextans', 'Sextans II', 'Triangulum II', 'Tucana I', 'Tucana II', \
+                    'Tucana III', 'Tucana IV', 'Tucana V', 'Ursa Major I', 'Ursa Major II', 'Ursa Minor', \
+                    'Virgo I', 'Virgo II', 'Virgo III', 'Willman 1']
+
+def count_lines_without_header(file_path):
+    with open(file_path, 'r') as file:
+        lines = file.readlines()
+    # Exclude lines starting with '#' (header lines)
+    data_lines = [line for line in lines if not line.startswith("#")]
+    return len(data_lines)
+
+count = np.zeros(len(mw_sats_1Mpc))
+i=0
+for galaxy in mw_sats_1Mpc:
+    satellite_name = galaxy.replace(' ', '_')
+    file_path_read = sim_data.home_dir+f'/orbit_data/hdf5_files/satellite_matching/combined_tweaks/floor_0.1_7_10/weights_{satellite_name}.txt'
+    line_count = count_lines_without_header(file_path_read)
+    #print(f"Number of lines excluding headers: {line_count}")
+    count[i] = line_count
+    i += 1
+list(count)
